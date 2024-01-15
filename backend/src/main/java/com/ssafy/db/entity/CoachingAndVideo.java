a30df1abe5fac3bdf332647dd02acf79d9173c66
김지원 <wldnjs35242@naver.com>
2024-01-15 14:58:11 +0900

    파일 이름 수정
    Former-commit-id: 3b97cbc6a7a02a013ea1d612cf511aed86ff3b1a [formerly 76c934b8fb1822709a8cc391edf593725ba5f54c]
    Former-commit-id: f2a19fa21dece89ca9bd1bbce4c6e84fdf8bca3d

---
+++ backend/src/main/java/com/ssafy/db/entity/CoachingAndVideo.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3227f65162398e44837a3c4dd112a0ea54619f206b19da7ca3d21fa98fa3868b
+size 323
---
+++ backend/src/main/java/com/ssafy/db/entity/MemberAndCoaching.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0f9acdbefd3447fa781280c8679bab65095cba3a612a91a84243ee72ff307838
+size 326
