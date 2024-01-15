6fc6402340911fc2dbdc7e8275901ba498812b72
김지원 <wldnjs35242@naver.com>
2024-01-15 16:27:31 +0900

    feat: @Column 추가
    Former-commit-id: 8d4f9b191fc75f2b7a1303811df719f40dd71ec7 [formerly 1bd7de8d40ee785b1acd480d526553e4fabc9302]
    Former-commit-id: e3c6cb02624952c20f896e8da46c8a6397d3e096

--- backend/src/main/java/com/ssafy/db/entity/CoachingAndVideo.java
+++ backend/src/main/java/com/ssafy/db/entity/CoachingAndVideo.java
@@ -1,21 +1,3 @@
-package com.ssafy.db.entity;
-
-import jakarta.persistence.*;
-
-@Entity
-public class CoachingAndVideo {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer coachingVideoId;
-
-    @ManyToOne
-    @JoinColumn(name="coaching_id")
-    private Coachings coaching;
-
-    @ManyToOne
-    @JoinColumn(name="file_id")
-    private Files files;
-
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:62282e93a20b3fb39b8ab24e9c40d1a97934769058f04dac24fb5422ec7d1527
+size 430
--- backend/src/main/java/com/ssafy/db/entity/MemberAndCoaching.java
+++ backend/src/main/java/com/ssafy/db/entity/MemberAndCoaching.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db988ec3b434f7dc78aee5b66b6a65a7fb904259ede31a2574f16a7c1219ee94
-size 552
+oid sha256:b5fcefcb250c2a67c360d7ee39a41990032e8652b7d46b16ec1c6668e3b423fb
+size 589
