21fb7375e0c3211211f91e0d9b80b70895458a97
HyejiLee <lee02g29@naver.com>
2024-01-15 14:04:17 +0900

    S10P12A403-229 feat: quasar 기능 추가
    Former-commit-id: 7ac164c3317be32514b87aa9aef5b0c95d013527

--- frontend/coachme/src/App.vue
+++ frontend/coachme/src/App.vue
@@ -1,3 +1,10 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:859ffdf91f5af9fa639e23f3d1428b47fd6c607419df5ba37786cc04f32a5219
-size 1372
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style scoped>
+</style>
