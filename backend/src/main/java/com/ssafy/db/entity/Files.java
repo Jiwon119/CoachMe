399144da1511cde5c3644f6d74a6c5c879e2233e
김지원 <wldnjs35242@naver.com>
2024-01-15 16:09:56 +0900

    feat: Files, MemberAndCoaching 수정
    Former-commit-id: d21a81ad0efa17e8e42c8fc72b5b33066b2fd4ab [formerly 4d00dc2440fdd3d8e3841afd91f9cf10cd3a8749]
    Former-commit-id: 443974539b79a4e75854a441cb02ceea96dff44e

--- backend/src/main/java/com/ssafy/db/entity/Files.java
+++ backend/src/main/java/com/ssafy/db/entity/Files.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf11b3bfc5be73cd80adf059c217caed8c2e64541edc5fc3d4eb2d791cd5b586
-size 694
+oid sha256:f51e9739cdf29812e82e641e93c888bbaea8bc2c5d6033124ce74329e5d07781
+size 734
--- backend/src/main/java/com/ssafy/db/entity/MemberAndCoaching.java
+++ backend/src/main/java/com/ssafy/db/entity/MemberAndCoaching.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3fe12676d933828988bf40d32094f81147fb07c07a47423f2d0ffa74dd635a1d
-size 516
+oid sha256:db988ec3b434f7dc78aee5b66b6a65a7fb904259ede31a2574f16a7c1219ee94
+size 552
