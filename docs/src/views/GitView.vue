7827dc93200a1e5f40f9eb580ba0a006bd04f1c3
Sangyeop Nam <spearoad15@gmail.com>
2024-01-15 11:16:59 +0900

    docs : 지라 이슈번호 관련 문서 변경
    Former-commit-id: 39083a8f2757c4cf560333bcd51a9cdbd933eb67

--- docs/src/views/GitView.vue
+++ docs/src/views/GitView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:158367dae6ef17d47548f1237d2e76085eb85fd6f4d46f2300a3e8f738d689a3
-size 23479
+oid sha256:2b04223fa9e175260cda3cf02f08628467cc7575d3c3636e904fe3d63c059ed4
+size 23621
