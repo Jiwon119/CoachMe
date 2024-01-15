830cad1fec112fa01110a8a5c51bcc5eef85a339
minahpark42 <minahpark42@gmail.com>
2024-01-15 16:27:37 +0900

    minah entity finish1 edit
    Former-commit-id: 4256ffaf42d53183f856f55380b8146923742b4f [formerly 14db50db2e8ca024c3d06ff325d402c398dce5bb]
    Former-commit-id: 07ee8f5568c3e93c048c86d80996997e4b65698d

--- backend/src/main/java/com/ssafy/db/entity/CoachingAndVideo.java
+++ backend/src/main/java/com/ssafy/db/entity/CoachingAndVideo.java
@@ -1,21 +1,3 @@
-package com.ssafy.db.entity;
-
-import jakarta.persistence.*;
-
-@Entity
-public class CoachingAndVideo {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer coachingVideoId;
-
-    @ManyToOne
-    @JoinColumn(name="coaching_id")
-    private Coachings coaching;
-
-    @ManyToOne
-    @JoinColumn(name="file_id")
-    private Files files;
-
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:a8f0ca2f5fae59e36cf04dec8cc81e604951c2dbea724653e2320307a7ffafea
+size 360
