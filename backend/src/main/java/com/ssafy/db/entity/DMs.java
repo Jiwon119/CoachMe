a087745019d3283c190bfc81e384eeaa6a37bb18
이준학 <0710june@naver.com>
2024-01-15 15:20:30 +0900

    fix: 엔터티 작성 완료
    Former-commit-id: 4ef645590f348af84d57407e9b5cd6d86029fd43 [formerly 5fbf8f1b6daa641b2692c35e4f7bb5b6b9ad3a4f]
    Former-commit-id: c0784113fe226ba5c77fa8d8bacf7649896b7e88

--- backend/src/main/java/com/ssafy/db/entity/DMs.java
+++ backend/src/main/java/com/ssafy/db/entity/DMs.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:384d66f3fde2cb678ae925d8b08030d73ee30f297e274c8e5d8c69cea7362400
-size 220
+oid sha256:3447005eea1c5c327cf839896189882cb5a7800cd11660db19370a24a5bc5ae5
+size 500
