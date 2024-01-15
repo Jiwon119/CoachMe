c988c83c0ddb0fe60187e0b03dc7cdb3957de9c1
이준학 <0710june@naver.com>
2024-01-15 16:07:29 +0900

    fix: MemberService 수정
    Former-commit-id: 1fa9e0e8ebe4d46a463b335bf8404c4d38e5e5a9 [formerly a780172aece556e7213154e69fe09c451246fdc2]
    Former-commit-id: fc41707adfa60f5081c6627b07cadf7b4cc475ad

--- backend/src/main/java/com/ssafy/api/service/MemberService.java
+++ backend/src/main/java/com/ssafy/api/service/MemberService.java
@@ -1,17 +1,3 @@
-package com.ssafy.api.service;
-
-import com.ssafy.api.request.RegistMemberDto;
-import com.ssafy.db.repository.MemberRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class MemberService {
-
-    private final MemberRepository memberRepository;
-
-    public void regist(RegistMemberDto registMemberDto) {
-
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:bb4645cbb29d2a5e6c6e9a8d38a77ff29930e98f600392d949264a04b1b881f0
+size 515
