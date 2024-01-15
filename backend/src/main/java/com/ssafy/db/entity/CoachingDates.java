4256ffaf42d53183f856f55380b8146923742b4f
minahpark42 <minahpark42@gmail.com>
2024-01-15 16:27:37 +0900

    minah entity finish1 edit
    Former-commit-id: 14db50db2e8ca024c3d06ff325d402c398dce5bb

--- backend/src/main/java/com/ssafy/db/entity/CoachingDates.java
+++ backend/src/main/java/com/ssafy/db/entity/CoachingDates.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:010bc83900ad9df427634db3a1fcbef6947996d3cd20b70741ca4fabbe946b95
-size 535
+oid sha256:67911f5922cc426dda879a6bc4bb6bd4996fa1530362716e1f80c280d230934d
+size 529
