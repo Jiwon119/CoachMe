919a8a480cff47c3bb7f12172f138ce1195f3aec
KDH <silverstuff@naver.com>
2024-01-15 17:56:28 +0900

    feat 레이아웃 작성중
    Former-commit-id: e5dabd8bcae9475b35adc0e1035f69c47cce15bc [formerly 58da7520ea1b5ab6858ee6e7b23ff3a398b2905b]
    Former-commit-id: f5007a8bb46c2f0eaecfe81ea4fc0ab6e04a5f6d

--- frontend/coachme/src/App.vue
+++ frontend/coachme/src/App.vue
@@ -1,3 +1,21 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:859ffdf91f5af9fa639e23f3d1428b47fd6c607419df5ba37786cc04f32a5219
-size 1372
+<!-- App.vue -->
+
+<template>
+  <div id="app">
+    <MainLayout></MainLayout>
+  </div>
+</template>
+
+<script>
+import MainLayout from '@/layout/MainLayout.vue';
+
+export default {
+  components: {
+    MainLayout
+  }
+};
+</script>
+
+<style>
+
+</style>
