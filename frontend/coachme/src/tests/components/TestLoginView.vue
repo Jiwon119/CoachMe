6a4236d5c0eaf18f093c19a9ea2b000efd578cec
Sangyeop Nam <spearoad15@gmail.com>
2024-01-17 16:34:51 +0900

    refact : 로그인 util function member에 통합 - api 스펙에 맞게 function을 사용하기 위해 api별로 function 생성하는 것으로 변경
    Former-commit-id: 257252929bf93889821e794d8b6c7d5d69390796 [formerly 702822b539a53b205760ea1f8a004d96186b2a13]
    Former-commit-id: c31f4ae27ed31ae4ee92202387a5d2a0807928e3

--- frontend/coachme/src/tests/components/TestLoginView.vue
+++ frontend/coachme/src/tests/components/TestLoginView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d184515af03978bb353f20524a9ae9fc89617efaf16c57f5b3210fa52a74463
-size 1532
+oid sha256:9a115840b8870f8a013a12e66ea65d4ba4def64394695108c8d3a9f7293d9f8c
+size 1840
--- frontend/coachme/src/utils/functions/login.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2d71810ffb178c0d966c4b434ed9cc48df8bf2935920a187684d7f06b010fa52
-size 455
