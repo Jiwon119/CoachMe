da24c578ebe06f9fe61cb39b20e7bab8ae1120c9
Sangyeop Nam <spearoad15@gmail.com>
2024-01-17 14:25:12 +0900

    S10P12A403-339 test : 회원가입 요청 Validation 테스트코드 작성 - 회원가입 요청을 확인하기 위한 테스트코드 작성 - 이제 개발자는 회원가입 요청을 검증하는 로직을 테스트할 수 있음.
    Former-commit-id: a0a82b4eea9a45451f93d0659a5627c3219292e2

---
+++ frontend/coachme/src/utils/functions/member.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d9c4630b1a27fb60b57ee7e67d99c9adc347bc291f599329ffc0c571ac7e483b
+size 2533
---
+++ frontend/coachme/src/utils/functions/regist.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dc0396115d4b0a84ccdf50e94599d67d4a39cd3a4ebbc9f9694398d23280f9db
+size 516
