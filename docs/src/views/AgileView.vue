82bf1c737b47f50c023440f536155eb1f2e4c02e
Sangyeop Nam <spearoad15@gmail.com>
2024-01-17 10:51:37 +0900

    docs : API문서, Agile Rule, Git Rule 일부 수정
    Former-commit-id: a901f95d05f3cb951f2a6665a03e034d3032cb18

--- docs/src/views/AgileView.vue
+++ docs/src/views/AgileView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77aef70c0ffbdbf7b0c092493f5aa466308c7beb2c181faff770bc4e8e20c918
-size 4487
+oid sha256:7b1b2406c1d8fa992ee9fa2a0c6d898df0889b0c346e8cd8fc6ee1a879b23831
+size 4862
