2fa5327aca0c7c407924944b72b9bed49f2bc0b3
SangyeopNam <spearoad15@gmail.com>
2024-01-19 05:32:21 +0900

    init branch
    Former-commit-id: 329dff283b78ff746553f85e5e67b3a749d9a9ba

--- frontend/coachme/src/tests/components/TestProfileImageUpload.vue
+++ frontend/coachme/src/tests/components/TestProfileImageUpload.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:239069ee76ada981e500782c1432a49114a6d1126405ec942cabc1f81b5b52b2
-size 1121
+oid sha256:3b15c7d6f98121f0f85b4e213b2ad29ccdc7a920aea716cc48e4b95698674e4c
+size 1090
--- frontend/coachme/src/tests/mocks/worker.js
+++ frontend/coachme/src/tests/mocks/worker.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0e26c727d1dc0ed872144767ee349637cd0424fbfbc9749d64368fcd21445ac
-size 1371
+oid sha256:93ceb6dc2582518cb6c3609cdb33879b0f679c4972bea9fd0dc7a53d93b92552
+size 1509
