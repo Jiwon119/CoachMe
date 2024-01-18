f179363fe5d4fe75e464029e49af27e4442d00a1
Sangyeop Nam <spearoad15@gmail.com>
2024-01-18 10:58:17 +0900

    S10P12A403-341 feat : 회원가입 기능테스트 구현(MSW) - 회원가입 테스트용 컴포넌트 생성 - 테스트 컴포넌트로 검증 완료 - 로그인, 회원가입 검증로직 일부 수정(Throw Error -> true/false) - 이제 개발자는 회원가입 기능을 테스트할 수 있음.
    Former-commit-id: 1af2ec803afff6258f49cbdcdd875b83ac5e31bc

--- frontend/coachme/src/utils/functions/__test__/regist.spec.js
+++ frontend/coachme/src/utils/functions/__test__/regist.spec.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6808ce8f6d474629d871bb4148c81c59a60afd9ccf055797fd7d9061a3e9028
-size 2950
+oid sha256:1d5148959b716a52bf4e21858b00278080cfda551281a38859afdd9614fbae82
+size 3731
