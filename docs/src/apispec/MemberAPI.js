d7c3163c6d20e834bb10caef5884f424dd161ff5
Sangyeop Nam <spearoad15@gmail.com>
2024-01-18 13:08:53 +0900

    docs : 비밀번호 찾기 API 수정
    Former-commit-id: ad0b569ef5a057aa43e9bb91b411526e6c48a005

--- docs/src/apispec/MemberAPI.js
+++ docs/src/apispec/MemberAPI.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:199164502e0899943c3a731682f3359ac5a2394d2ea67814559d44e3ac178d68
-size 9145
+oid sha256:b0c1ae68b3cf2325f67db3decdd5c58cf6d85b94c3a6a71c382e7579acd3e010
+size 9324
