9fbeeedf1875a9512b4f9e1334a4bf52a2cb6d39
Sangyeop Nam <spearoad15@gmail.com>
2024-01-18 13:23:09 +0900

    S10P12A403-301 feat : 비밀번호 찾기 기능테스트(MSW) : 비밀번호 찾기 후 정상 변경되는지 확인 - 비밀번호 찾기 요청정보 확인기능 정상 동작하는지 확인 - 이제 사용자는 비밀번호 찾기를 통해 본인의 비밀번호를 재발급 받을 수 있으며, 개발자는 비밀번호 찾기 기능을 테스트해볼 수 있음.
    Former-commit-id: 197236bc3875615bc0adea34b0f796e642874720 [formerly 4275b903b98845fdeb6cf5439d0d48d277acbce4]
    Former-commit-id: 6d5ba2ce28bf98a7de1c54331a265a567a024b0a

--- frontend/coachme/src/stores/member-info.js
+++ frontend/coachme/src/stores/member-info.js
@@ -1,21 +1,3 @@
-import { defineStore } from 'pinia'
-
-export const useMemberStore = defineStore('member-info', {
-  state: () => {
-    return {
-      memberId: '',
-      privilege: '',
-      accessToken: '',
-      refreshToken: ''
-    }
-  },
-  persist: {
-    enabled: true,
-    strategies: [
-      {
-        storage: sessionStorage,
-        paths: ['memberId', 'privilege', 'accessToken', 'refreshToken']
-      }
-    ]
-  }
-})
+version https://git-lfs.github.com/spec/v1
+oid sha256:590419930c8f6c3a2ecc2a32763e7d41dd174f8ff768329c054441eedc952de6
+size 383
