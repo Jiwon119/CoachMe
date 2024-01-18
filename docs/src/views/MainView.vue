aeda30afa179e53a50773559b3845ca2a6c307b1
Sangyeop Nam <spearoad15@gmail.com>
2024-01-18 09:45:10 +0900

    docs : 텔넷 관련 도움말 추
    Former-commit-id: 9425a8c3a25563f87505e4e4b797a1958693bb8b [formerly 48d38c0a8f4510a45e15cd37b65ae338df1b0e6c]
    Former-commit-id: b36176cc74397292273c5beb9daea863e8eded6f

--- docs/src/views/MainView.vue
+++ docs/src/views/MainView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e4f78f355ccbc8d7d320b0d1e080887f70b6856d16606d8f08bfbd3734d074b
-size 1665
+oid sha256:b8f8ebd0be95c76e2d3ee0d51dc11eeadf481656667b1a49d3bda00178eb11ad
+size 1696
