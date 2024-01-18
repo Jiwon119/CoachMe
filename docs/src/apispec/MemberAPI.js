a1cd796e666f1104475e957ed904319414aa49cf
Sangyeop Nam <spearoad15@gmail.com>
2024-01-18 10:18:59 +0900

    docs : auth api 추가
    Former-commit-id: 88fb0b8060970d0ac3c1d9940dddfb9de3706856

--- docs/src/apispec/MemberAPI.js
+++ docs/src/apispec/MemberAPI.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe629404ecbfa524c6c5e1a2dc15d97990ab5bbd5cae422d42087e599934b1ff
-size 9166
+oid sha256:199164502e0899943c3a731682f3359ac5a2394d2ea67814559d44e3ac178d68
+size 9145
--- docs/src/views/MainView.vue
+++ docs/src/views/MainView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8f8ebd0be95c76e2d3ee0d51dc11eeadf481656667b1a49d3bda00178eb11ad
-size 1696
+oid sha256:3c61bdf75909243ed8314df4bd2f4c91cbfd8e295b025928ea4cdaeb5952d75e
+size 1723
