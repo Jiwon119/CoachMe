d7f7f6fb9e7883d5252324e1a43586ba8f5417ab
Sangyeop Nam <spearoad15@gmail.com>
2024-01-18 12:55:27 +0900

    S10P12A403-298 feat : 비밀번호 찾기 API 구현 - 비밀번호 찾기 요청을 보내는 API 생성 - 이제 사용자는 비밀번호 찾기 요청 API를 전송할 수 있음.
    Former-commit-id: 523bd7ff411c2f5929004b5f7fa61eac9f7893ab

---
+++ frontend/coachme/src/tests/views/TestChangPasswordView.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1748a7297168931a5f8354c31cc54a5cf5ed4e8a8e1474096361b82b7cb4f2fc
+size 1548
--- frontend/coachme/src/utils/api/dto/member-dto.js
+++ frontend/coachme/src/utils/api/dto/member-dto.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:950a63d5de8de37f6c61f91167fef7b7a0c78c84d57c7d0b2db5714f2564122c
-size 593
+oid sha256:8836cbc4f1f72ab7f1f9df45cba31627c0cf185bfd7a8749a22ceac95184d3a3
+size 842
