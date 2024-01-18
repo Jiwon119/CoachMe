813fc77fe277fe203322581c47dd0f4faf5cac4f
이준학 <0710june@naver.com>
2024-01-18 12:43:24 +0900

    S10P12A403-272 feat: 로그인 API 구현
    Former-commit-id: 911e7493283b4048189deaeefe45fb8d5507ff0f

---
+++ backend/src/main/java/com/ssafy/api/auth/service/CustomUserDetailsService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a7f6f20149f125861a3b3e58d3c2f59a2c88c6e28d64210fddf097470ce57959
+size 1103
---
+++ backend/src/main/java/com/ssafy/config/SecurityConfig.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5cb9ba89c685b46e7e914262983071489efb987062dd991ed1b99bbccdd23c1a
+size 2331
