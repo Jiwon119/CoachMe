8b4db68055d95abab92d8d490d3dec90b8424d8e
SangyeopNam <spearoad15@gmail.com>
2024-01-19 05:23:47 +0900

    docs : 프로필 사진 수정 API 일부 변경
    Former-commit-id: 7095580b4611a3d9527ec1c8fd3a4545cd8909de [formerly 57d92c2a24adf915c0f75ad137171bfa3f628c96]
    Former-commit-id: 7afc25197100a825b3c4a0f0c81de216a834634a

--- fileserver/server.js
+++ fileserver/server.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ebb8e87418885225ec9ae0982c3ca3975c5d6536a44610e9045c6b9976d4e32
-size 1934
+oid sha256:c669458b700ee6c65147714f6d0d389cb77c178ce8b2e4fdbd11d428bf0db38e
+size 1966
