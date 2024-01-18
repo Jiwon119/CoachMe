e610fd8752e134665ab730da1079f20df155766b
Sangyeop Nam <spearoad15@gmail.com>
2024-01-18 17:44:50 +0900

    docs : 회원탈퇴 API 수정
    Former-commit-id: e5a33d4d2d6b802aa3d4d3c3d3c9d5579de86628 [formerly 0475d42808a2214df358f19cf3f0187006b49ba7]
    Former-commit-id: c8cd80a14adb00daa137dd5f0cb2cd969fe84ce0

--- docs/src/apispec/MemberAPI.js
+++ docs/src/apispec/MemberAPI.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d53ab44bd6737586aebe4a054e5af4321bbff1b7a2f3f2cb9a18e5088382e2b
-size 8681
+oid sha256:8fc78d7d18377698bd8df5074a1f24c55ca5bee077ad310656f3925793f61d12
+size 9192
