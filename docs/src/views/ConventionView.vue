a0ae6f5973c6687a543d6c8ffcf836ff03a51cca
Sangyeop Nam <spearoad15@gmail.com>
2024-01-19 09:09:57 +0900

    docs : 미들웨어 관련 프론트엔드 컨벤션 수정
    Former-commit-id: 803ddbe9345af2fc1b9b9e485e2a9d6a6272b9f6

--- docs/src/views/ConventionView.vue
+++ docs/src/views/ConventionView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c86b3fe14e23a2d57de51f8c884d7ec545b6263b215a9fb73fdb714758fe3b86
-size 8735
+oid sha256:9d3e3f557b4911dc6b20959b06190c1d60bcca768d711091d16fbc50b5c29a56
+size 8880
