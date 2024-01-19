1a738bc1a466f9557ce63bf3e91b4e5afc6f95a4
Sangyeop Nam <spearoad15@gmail.com>
2024-01-19 09:58:27 +0900

    feat : 권한별 네비게이션 가드 추가
    Former-commit-id: 8990bd0d4a340eab791809f4f62aa84d8c1d85e2

---
+++ frontend/coachme/src/router/middlewares/test-before-guard-privilege.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2e3ccbbd96b787120ecb00c2b88f623612d5f53ac93b2bbd53fe6ea831fcab63
+size 975
--- frontend/coachme/src/tests/views/TestProfileView.vue
+++ frontend/coachme/src/tests/views/TestProfileView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d48c1dfad22778fcd048e94f638a109167ea6d87edeb9a49195beb4d3f92694
-size 2830
+oid sha256:853ff41e6af8994c648cfc502119a89b03ed5a9f3ac9f62c7bd0a3f8599cf839
+size 2775
