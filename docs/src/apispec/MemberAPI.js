0d2742467d35a5bee5c6e7cffae0b21dbe14d000
Sangyeop Nam <spearoad15@gmail.com>
2024-01-19 12:40:40 +0900

    docs : Member 조회, 수정 API 일부수정
    Former-commit-id: 5d79153b3ac5337ff766d0ef0950278a069df7ec [formerly d7cc7d33fbed10b856d1dec8204df6dc88208245]
    Former-commit-id: 7ece8e36b04badbe82ff7cbd3dc0b24dc30ee815

--- docs/src/apispec/MemberAPI.js
+++ docs/src/apispec/MemberAPI.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab4e48fb9b987fdcc9d96e5afc680bb4e531c71cfc451a79b5fa81e8b69dde61
-size 9208
+oid sha256:08012c1be52d2f86ebd74993eaf70774bf054c584789c3ab5be7065047bcaed1
+size 9290
