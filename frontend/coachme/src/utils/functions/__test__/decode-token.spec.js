18112982b499fc0ede1d2728d2ea21cba665b774
Sangyeop Nam <spearoad15@gmail.com>
2024-01-19 11:08:49 +0900

    feat : 토큰 받아오는 함수, 토큰 파싱하는 함수, 토큰 정보 확인해서 Router에 진입여부 확ㅇ니하는 기능 추가
    Former-commit-id: 57f928395612855d1bf3314552e8c6c4a7a06ab7

--- frontend/coachme/src/utils/functions/__test__/decode-token.spec.js
+++ frontend/coachme/src/utils/functions/__test__/decode-token.spec.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0b0c65a1a5b7b87c8a14eb6bf56210d64f1649fdf2f01b83d9cf84201b3bfee
-size 1207
+oid sha256:73a2ba4b142426cf2e26b3300a41995673c187db75d4ce162331d7f5ea99e4d8
+size 1358
