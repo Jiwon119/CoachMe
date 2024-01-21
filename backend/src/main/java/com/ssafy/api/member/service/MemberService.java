10ad4ecb0fa9643c1527d6e3f68493256ce83919
이준학 <0710june@naver.com>
2024-01-21 20:39:38 +0900

    fix: Merge 후 발생 버그 수정
    Former-commit-id: c45a43bb888c9a9ecd3a20eab7fe83484e3c1d34

--- backend/src/main/java/com/ssafy/api/member/service/MemberService.java
+++ backend/src/main/java/com/ssafy/api/member/service/MemberService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:054ef11deadc3aac225637f0b1195e69383bbca7ecfe8cb9ce96ddbd11a845bd
-size 2572
+oid sha256:ac10365cec23f2a0de5b50d171c2ad589b0bb2b6dc89342eca0694d11b8e9346
+size 2365
