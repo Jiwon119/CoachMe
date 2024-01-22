c58e816e62f75aaae257814b8071f066515da0a3
이혜지 <lee02g29@naver.com>
2024-01-22 17:50:15 +0900

    S10P12A403-400 feat : dm 리스트 및 dm 1:1 대화창 기능 추가
    Former-commit-id: ea3c89dbc7d93d668f1813a0d9b7f678b6f4d5ba

--- frontend/coachme/src/components/molecules/DmWindow.vue
+++ frontend/coachme/src/components/molecules/DmWindow.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2fbbf1273f4fa3463546c413d74cb7fed89db03baadbae314de73244d5f24884
-size 887
+oid sha256:62486875144afa7ce3e586f85b813640d9c91ff5188011e8b3e212cad0033254
+size 1188
---
+++ frontend/coachme/src/components/molecules/SelectCoach.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:98c1996b2d310f4080586c79eca8b6e9130d3a5fb9ed27361c3262168f1cfd61
+size 1256
