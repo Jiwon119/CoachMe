05e586d7eb9ae282fd316476f2be44e775b2ae7b
Sangyeop Nam <spearoad15@gmail.com>
2024-01-22 14:04:13 +0900

    docs : admin api 일부 수정
    Former-commit-id: 01211e5d9b022429c0ab3e5966e9b0a1bdad8146 [formerly 5b76b9d07b7f7918310146377034a0ea955117ba]
    Former-commit-id: a7b65e5c36866c75c40edcad23da7786bd461956

--- docs/src/views/ServerView.vue
+++ docs/src/views/ServerView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fef07388173a32fab13fd8c580fc88ed62b1db538f33b03e62f01761698ceb66
-size 4651
+oid sha256:dc99ca8988f3a360f4ab2757998d9509f8fc3b697d4467054e59340747d4c1cd
+size 4699
