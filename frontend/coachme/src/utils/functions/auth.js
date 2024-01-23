6a6b550127141f64e17fee13c892cc460c3c18d0
남상엽 <spearoad15@gmail.com>
2024-01-23 17:29:47 +0900

    S10P12A403-163-핵심-부가-관리자-admin-는-권한-privilege-상승리스트-list-를-확인할-수-있어야-한다/feat/2
    Former-commit-id: a21a7df9f776162ea79d7cbd8320d4013bf7751b [formerly c2dc7c4af1b0085227b95bd252b6fda37465aca2]
    Former-commit-id: 4cb01a963e8d7df7174b1718a5b1b04a078691a5

--- frontend/coachme/src/utils/functions/auth.js
+++ frontend/coachme/src/utils/functions/auth.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea06dbf3ad5579806e5d86ca3908456ab50ed588e1d1a6239b7a6588e98b86ec
-size 3277
+oid sha256:3a1ba97bebd8ba1e3858d9049a43786551e14ea1731b11a71c68aea25a8d2e44
+size 3411
--- frontend/coachme/src/views/mypages/MypageMainView.vue
+++ frontend/coachme/src/views/mypages/MypageMainView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e7bdf82870fc5f8eaa81154a08d15d555a9d0db572a7ebc4ebcc6036927a87f
-size 2630
+oid sha256:2fd5d64156e9e1a0ef72325739fd04e21a5d229782a6ecea5bfccaf3d48ec609
+size 2952
