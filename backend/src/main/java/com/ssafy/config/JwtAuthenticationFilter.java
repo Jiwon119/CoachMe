2885dd1db076955ee2cc5a9d8a304522e9243f3a
이준학 <0710june@naver.com>
2024-01-24 21:54:15 +0900

    fix: filter 수정
    Former-commit-id: 0e4446e6b9d034c812a8f0a1d2ba6d8120186c0e [formerly 7d17afb82429589e09b575937a174688bd56a997]
    Former-commit-id: 3c1af6d42d6926e46f479611481a43b8ae95d94d

--- backend/src/main/java/com/ssafy/config/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/ssafy/config/JwtAuthenticationFilter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d87a893e3e442cc5d7e26f6611895a1f648a1458bbb7ec3693c33b9f60d5fe2b
-size 4348
+oid sha256:455d530f3b47bea6b1f2b27cacd524f5375a87cc43429a208d84730e5a4c729f
+size 4384
