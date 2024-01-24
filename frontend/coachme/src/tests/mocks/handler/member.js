abff86664c5dbf41b8c87a5f67b531931c29df8f
Sangyeop Nam <spearoad15@gmail.com>
2024-01-24 17:58:58 +0900

    fix : 오류 수정
    Former-commit-id: ad27179a99361f60415f7242d2d0a1c3c7851385 [formerly 07df4b2a07921f4971898c6503da48a1c0ecdd21]
    Former-commit-id: dadbcfe9fb027cb166314cab89b20be046d108f6

--- frontend/coachme/src/tests/mocks/handler/member.js
+++ frontend/coachme/src/tests/mocks/handler/member.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:642023c9983420471b721201343cf36a7130bedd678efcf2cd4b380f5ddebd07
-size 2449
+oid sha256:7842cf4cb5e2c1ccad9e3242b3aa0f14686084bc52f13a476f7182d37ff768df
+size 1657
