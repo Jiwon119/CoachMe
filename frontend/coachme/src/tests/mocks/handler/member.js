6be1b3c1577346e48ffe170f23078bb5f28c38c9
Sangyeop Nam <spearoad15@gmail.com>
2024-01-24 16:50:32 +0900

    fix : login api 경로 수정
    Former-commit-id: db46295da0b1463685f5176c2d9ed635dc52d702

--- frontend/coachme/src/tests/mocks/handler/member.js
+++ frontend/coachme/src/tests/mocks/handler/member.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:438e6a9c7ac0018f2db745cf4a5e163949633124dccb0f1d642f0dcfc9bdcd09
-size 2829
+oid sha256:642023c9983420471b721201343cf36a7130bedd678efcf2cd4b380f5ddebd07
+size 2449
---
+++ frontend/coachme/src/utils/api/auth-api.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:66b9ece9f8207d0bef4343b6d4cd109c0f4a7d8b99fa9f4d1958b73c5b615d83
+size 709
