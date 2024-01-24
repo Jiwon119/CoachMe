94a843acff6848cba86db8c0bf76d5fc3d3b0a84
Sangyeop Nam <spearoad15@gmail.com>
2024-01-24 13:58:43 +0900

    docs : 프로젝트 시스템 아키텍쳐 탭 생성, 회의 ppt 회의록에 업로드
    Former-commit-id: 6616bced88149549ffb67e734fae470d790b13e0

--- docs/src/apispec/AdminAPI.js
+++ docs/src/apispec/AdminAPI.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d69814689643e6e18025a0c0c231ba9ad8757d9626fd91013aeccbfb027eee5a
-size 2238
+oid sha256:d065ce3b988498981dfd227f1e84b7cfaa3c5f5a60ae5145c7036650a1b06e69
+size 2179
