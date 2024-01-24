aa4a56544b0caff43fa8386c7afa1894336346ca
이준학 <0710june@naver.com>
2024-01-24 21:13:33 +0900

    fix: CORS 수정
    Former-commit-id: 9d1c338f9ee5d73d528eaee6a811d248dd332ecc

--- backend/src/main/java/com/ssafy/config/WebConfig.java
+++ backend/src/main/java/com/ssafy/config/WebConfig.java
@@ -1,16 +1,3 @@
-package com.ssafy.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**")
-        .allowedOrigins("*")
-        .allowedMethods("GET","POST", "PUT", "DELETE", "OPTIONS", "HEAD");
-  }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:4acf069dab26c76fa2d47b7a56849858d07d1bc0a53c20f2715b90d613692814
+size 620
