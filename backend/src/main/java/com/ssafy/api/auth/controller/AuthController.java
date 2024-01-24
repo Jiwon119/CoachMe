92b8f365f929de57156d29f51d8f4734ce92f937
SangyeopNam <spearoad15@gmail.com>
2024-01-24 22:32:14 +0900

    fix : cross origin 정책 추가
    Former-commit-id: 409e7e670b87b9c11eb27d37da8da678ed64fb9f

--- backend/src/main/java/com/ssafy/api/auth/controller/AuthController.java
+++ backend/src/main/java/com/ssafy/api/auth/controller/AuthController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1a7323514ef936ec51908675923788fd60fab6fbb9c5fdcb15001c3cc6c08e0
-size 2459
+oid sha256:25ee18d98a632cfb34cbc0e43cf27d2a171be14a4d1661ecb28004f156376a9e
+size 2301
