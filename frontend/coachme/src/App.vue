a4bcd926557a2000017e0ffcfe65a09b9d01ce67
Sangyeop Nam <spearoad15@gmail.com>
2024-01-24 15:48:22 +0900

    fix : 내부 ip주소 사용
    Former-commit-id: 17521265e8d67290e2d47e2e936182e539e4bb31 [formerly ca1e51871ea2b636687de211c0fdb6ffd383a9d3]
    Former-commit-id: f18f83460d3bef2e1c64d95a510ff53657c169d7

--- frontend/coachme/src/App.vue
+++ frontend/coachme/src/App.vue
@@ -1,3 +1,9 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:dc09ec53563fa0084f20ec3684d1856aed6b5818cc097d7062a43b17045f5fb3
-size 104
+<template>
+  <div id="app">
+    <RouterView />
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
--- frontend/coachme/src/tests/mocks/worker.js
+++ frontend/coachme/src/tests/mocks/worker.js
@@ -1,3 +1,5 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:03d2439d6254610e450ed7a252c9bb2a1d4fac7b65f12642dbb9656cd03b8b89
-size 200
+import { setupWorker } from 'msw/browser'
+import memberHandler from './handler/member'
+import adminHandler from './handler/admin'
+
+export const worker = setupWorker(...memberHandler, ...adminHandler)
