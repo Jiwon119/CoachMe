724a797228db233b263d58582d3c3e9a06c9a9c8
이혜지 <lee02g29@naver.com>
2024-01-25 10:07:50 +0900

    S10P12A403-401 fix : module 오류 해결
    Former-commit-id: 3c6431c57847c16411a8d54ab9c480d71840b6b6

--- frontend/coachme/public/mockServiceWorker.js
+++ frontend/coachme/public/mockServiceWorker.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5d6829cd20c2526ad19dd3f5757246f05b203b804df1915e67853dcad23b4ed
+oid sha256:390b05b09f16329d1192ff4720b81c69db29b79e89cb1c7b530cdc3f3c422b35
 size 7672
