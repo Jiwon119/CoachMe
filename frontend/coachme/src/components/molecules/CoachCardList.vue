ec43b847f26208db05644bb87e2834c0df5f9f46
이혜지 <lee02g29@naver.com>
2024-01-25 09:39:39 +0900

    S10P12A403-398 feat : 코치 카드 채팅하기 버튼 추가
    Former-commit-id: 685ecd301d995df324716182259c79fd0de7285c [formerly e0997f6b4077f53cd752b2363f2a6b9cc00d0f52]
    Former-commit-id: 506f6f31035b4732e08274f6cb01eba757a4651b

--- frontend/coachme/src/components/molecules/CoachCardList.vue
+++ frontend/coachme/src/components/molecules/CoachCardList.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8725dc8d94ab1bf814f14f8db78f450991b1ad37d61e1cb5e2f5cc974e68688
-size 9805
+oid sha256:b847122acbe48d90b3e9101eb82282973c1b535dd151cca9d5a3e54e3af5489d
+size 9711
