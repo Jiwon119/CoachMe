0ad150579e67f8b70a677d0a1bb1ffea20fc18cd
Sangyeop Nam <spearoad15@gmail.com>
2024-01-25 09:52:59 +0900

    fix : cors 추가
    Former-commit-id: 6acbe78c49ef53d466733c154142524e53a4b898

--- backend/src/main/java/com/ssafy/config/WebConfig.java
+++ backend/src/main/java/com/ssafy/config/WebConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd8740997eb0d7a9d0368c07bac5d7b08529408f243bff56e6e8bac84fa38226
-size 656
+oid sha256:6c5450b9d94d4b1461142b01ecb459490dc8c90604cc5b954935c5db437bfddc
+size 571
