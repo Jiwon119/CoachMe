230b1d3b82459be2f881c2645579c2724fdc0313
이준학 <0710june@naver.com>
2024-01-25 09:40:59 +0900

    fix: cors 설정 수정
    Former-commit-id: 45c04e2605ac26733f17011f00a2ddeb2ee833a8 [formerly cf0dfef617ceede717fe57392f2b703823b6ddae]
    Former-commit-id: 9d9b760a31b3dd9e8f6fce17b4a37c45374854af

--- backend/src/main/java/com/ssafy/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a687dea0408463be7ac4e3c10fa26904f4f9d254338f25c15d88f7fa6d3945d5
-size 2478
+oid sha256:7da8cdcafcebb7ea70c0b4c6a626c74926c409d466a9cec44acae437edbf8c10
+size 2525
--- backend/src/main/java/com/ssafy/config/WebConfig.java
+++ backend/src/main/java/com/ssafy/config/WebConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4acf069dab26c76fa2d47b7a56849858d07d1bc0a53c20f2715b90d613692814
-size 620
+oid sha256:bd8740997eb0d7a9d0368c07bac5d7b08529408f243bff56e6e8bac84fa38226
+size 656
