6741584f735c5ad9864c1bb06c8b279c79fbf3a9
SangyeopNam <spearoad15@gmail.com>
2024-01-27 05:29:08 +0900

    manually resolve merge
    Former-commit-id: 40703b5dcc7a0c01cdee5b73a3d1d14b6bd12efe [formerly 7079eb5665c223d3af625a0bcdfd9e450719195b]
    Former-commit-id: 39c9951a1af6c3562c1c7c5469592673eba2ac18

--- frontend/coachme/src/views/RegistView.vue
+++ frontend/coachme/src/views/RegistView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:70bde4f3f2819c655598e4c555f55b96e0f3c3c810942f18a580188f1943905d
-size 8943
+oid sha256:774bcdb3ccd42e12dc86fcd458b7f24aba90c2755bdc983508718acfe43adcad
+size 9094
