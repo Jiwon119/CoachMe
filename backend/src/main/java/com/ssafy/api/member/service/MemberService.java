4f3441c0bbc15b9f7910c836b9433160d8edc5c0
Sangyeop Nam <spearoad15@gmail.com>
2024-01-26 17:55:07 +0900

    refact : controller 리팩토링
    Former-commit-id: 51d8cf7aea168d5e7c2b885a8d7f440ae120edbf

--- backend/src/main/java/com/ssafy/api/member/service/MemberService.java
+++ backend/src/main/java/com/ssafy/api/member/service/MemberService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d1d7aafceff593673a643ebaee17df411cc453dc701c408fdd5ea099cb54c0d
-size 1698
+oid sha256:6f88858e77118186d77e1bd454e02105610f4e3e500fe592dd77f38c9d509ff1
+size 1836
--- backend/src/main/java/com/ssafy/db/entity/Member.java
+++ backend/src/main/java/com/ssafy/db/entity/Member.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b0626a2f443cc7aaced4e02433fc9c6e1946b0abcfeb2e1c17b70824b032a3e
-size 3200
+oid sha256:8608bb2ca3c904727d19411bc65ca0debb51db1f2ae4e5a030c1653345525960
+size 3677
