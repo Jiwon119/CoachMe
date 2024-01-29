282195f4c85e511a1c0b448dcf7f0dc570ed9415
김지원 <wldnjs35242@naver.com>
2024-01-29 17:24:24 +0900

    S10P12A403-460 feat: 관리자 권한 상승기능 API 구현 완료
    Former-commit-id: 7c74e8e55e91503f4f789e0b1a5a5b8df70c88d5 [formerly 77a68cbe0278bd33fdef82e6a63c9982d095e7ce]
    Former-commit-id: 262e02c37dd2959b58fab5e2fcbc0e350f797ce9

--- backend/src/main/java/com/ssafy/api/admin/controller/AdminController.java
+++ backend/src/main/java/com/ssafy/api/admin/controller/AdminController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e282b9fc3acaec82e1da99fcbd4dc436c8d3f292fc1ec8574cad5aa6f2d2335
-size 1796
+oid sha256:c8f9f86e49fea7e2062c533488e4110e399d0389b06fe8a488e8a53f20d71b35
+size 2475
