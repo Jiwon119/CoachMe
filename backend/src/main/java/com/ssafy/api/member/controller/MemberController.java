fe69887e2f3d9c37a483981086ead7db6f80dcf8
Sangyeop Nam <spearoad15@gmail.com>
2024-01-29 14:09:34 +0900

    feat : 계정 권한상승 API 구현
    Former-commit-id: 00fc938e9c22f3a6dca8aca98cf4f1ed998c3604

--- backend/src/main/java/com/ssafy/api/member/controller/MemberController.java
+++ backend/src/main/java/com/ssafy/api/member/controller/MemberController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b773b4c8069c47c0a2998986f43a65b34bad965525d23d75b838ea7d4f39a32
-size 6895
+oid sha256:ac9fe64b66d72d4caa1206179bf552d79197333fbb3a9246a345d1d95a3674c7
+size 7578
