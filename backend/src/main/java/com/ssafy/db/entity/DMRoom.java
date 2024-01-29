86dd7e622f315276cf4d4ec02907f2c0fc426d91
Sangyeop Nam <spearoad15@gmail.com>
2024-01-29 12:48:09 +0900

    push to server
    Former-commit-id: cbfc34db922bcb54221c5dae2a46dd0d11b7bcaf [formerly 52a8e8c465e86df029d1ba198c4df3ddfb4f617a]
    Former-commit-id: b1522fe742d438b13dd7c133c5744e1edbf12906

--- backend/src/main/java/com/ssafy/db/entity/DMRoom.java
+++ backend/src/main/java/com/ssafy/db/entity/DMRoom.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ae516c9228a09e496ac925756642169c1ee8ec9e4f5295f40abf1ff1c2bef67
-size 538
+oid sha256:ce39dab12622683622438934bff0217adc68dbbb02689537111481d83fedc2ed
+size 532
