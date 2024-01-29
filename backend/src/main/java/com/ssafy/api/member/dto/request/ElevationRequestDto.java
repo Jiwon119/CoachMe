018de2392631040c3cf681ddd2708ea48ed3c3b6
Sangyeop Nam <spearoad15@gmail.com>
2024-01-29 14:09:34 +0900

    S10P12A403-446 feat : 계정 권한상승 API 구현
    Former-commit-id: 4f30b1b2f0ca1c4809b13a59f21918f7594b8c3c [formerly e5721d63800a3cf0fe3e3e6835e53e0dee2faf25]
    Former-commit-id: 1fed483372946bf1ee6ac913b707f957c4b98c4a

---
+++ backend/src/main/java/com/ssafy/api/member/dto/request/ElevationRequestDto.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ed08c30796adb4522ac6fb012fe51872d66594a1a1cb8a03d66ae00dead47c76
+size 224
--- backend/src/main/java/com/ssafy/db/entity/Portfolio.java
+++ backend/src/main/java/com/ssafy/db/entity/Portfolio.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2c9300f6a0dc01940616adfb991d3b18842ebb0627d570ac29ba6695a624ff3
-size 290
+oid sha256:159d0942532b2cb45d975eceb66f4886575455d5831a20cfa6af6eb75840c6b8
+size 445
