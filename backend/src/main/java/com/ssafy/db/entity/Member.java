9b07797353b7c9e323481a1d02aabfd1537dfe5a
Sangyeop Nam <spearoad15@gmail.com>
2024-01-29 15:47:58 +0900

    refact : Member - UserDetails 로직 분리
    Former-commit-id: a376d8eb6a79b0f6426c20742394165cd076e2a4

--- backend/src/main/java/com/ssafy/db/entity/Member.java
+++ backend/src/main/java/com/ssafy/db/entity/Member.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb12328ea9d7a47da2f7032ec7f63eefa77996573166685e1aab639677d462ca
-size 3900
+oid sha256:54dbff4f2141374fba2929a03de41282f9b44f2df07e10f6be68a496380908a4
+size 3378
