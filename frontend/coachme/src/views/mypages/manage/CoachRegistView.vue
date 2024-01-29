56ff507f0a6096ebf727be615543388210b47397
Sangyeop Nam <spearoad15@gmail.com>
2024-01-30 08:52:38 +0900

    refact : 미사용 파일 제거
    Former-commit-id: 920e689cd846e7ad323f9896c270487a4414024c

--- frontend/coachme/src/views/mypages/manage/CoachRegistView.vue
+++ frontend/coachme/src/views/mypages/manage/CoachRegistView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:980a16ce79f8323defe6c7c82dcec4d83db161b34fbc1128985a5beb7edc992d
-size 2664
+oid sha256:762acfcc97ba4478bbf26f0687de221a81301da9e69ad69905fbd3adf6f1ef3d
+size 2636
