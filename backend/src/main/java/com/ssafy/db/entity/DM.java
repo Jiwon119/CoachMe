cbfc34db922bcb54221c5dae2a46dd0d11b7bcaf
Sangyeop Nam <spearoad15@gmail.com>
2024-01-29 12:48:09 +0900

    push to server
    Former-commit-id: 52a8e8c465e86df029d1ba198c4df3ddfb4f617a

--- backend/src/main/java/com/ssafy/db/entity/DM.java
+++ backend/src/main/java/com/ssafy/db/entity/DM.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:663d7d6231b934c11cab83f8e92609d4fe509cf8367cdd706598cbc2e0787d25
-size 586
+oid sha256:96133ec286c5faa4b0ca9e78cdc412b4c04689787a816a409e0024826396e9dc
+size 584
