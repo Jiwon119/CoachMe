c79e4f64fa93340fc4791c22803357e463b398f9
Sangyeop Nam <spearoad15@gmail.com>
2024-01-30 10:14:13 +0900

    docs : 권한관련 API 일부 수정
    Former-commit-id: e2d3592e07e3350bc9a2c8451cf5ea52648278fa

--- docs/src/apispec/AdminAPI.js
+++ docs/src/apispec/AdminAPI.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e1729dbf021b7e902fdb836f134973a434df57523346d2f46c46c40390183cd
-size 2302
+oid sha256:f13d179196f54ae53f6efd1c00eb69cf4feaf672882b9c5ae99e077d7e551ccd
+size 2312
