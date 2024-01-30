69d70027e99162cc8a3f1d121a20ecf1732c969d
김지원 <wldnjs35242@naver.com>
2024-01-30 13:20:20 +0900

    feat 권한 상승 요청 api
    Former-commit-id: 609f7730396cf232253c5aa57f7b5dbbe3121c4d

--- backend/src/main/java/com/ssafy/api/admin/service/AdminService.java
+++ backend/src/main/java/com/ssafy/api/admin/service/AdminService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a61a2dc00c2535119ef8bed07e2b39ce9659565ca3d59bd074e38cf8ff6ae5e4
-size 1361
+oid sha256:ffbd470df17c2bd797eaa7c6b14f32f2c4725dba45af01fb4eecdda9591912ed
+size 1671
