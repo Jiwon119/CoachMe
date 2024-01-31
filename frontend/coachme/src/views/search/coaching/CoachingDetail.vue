a9c6ce87ec20e131a61482c1b28388f5af66588c
이혜지 <lee02g29@naver.com>
2024-01-31 17:47:50 +0900

    save : 작업 과정 중간 저장
    Former-commit-id: d3ce796524edfa6da8da08b7b78408530af5c4da [formerly 71c37627a9dc36dd04c7bdae5722f9622f2497a4]
    Former-commit-id: 721c33dcaf6d999d406580eb19fc144268f10161

--- frontend/coachme/src/views/search/coaching/CoachingDetail.vue
+++ frontend/coachme/src/views/search/coaching/CoachingDetail.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee059d3ce4e1d7e34da3338a29d33dfb420b86a828f0ed245e8ef6bc88ce77c0
-size 5080
+oid sha256:3c75063e016d4e74d2dc167feb95b2f3000041af442177b8fdddecfcf1408c3f
+size 5104
