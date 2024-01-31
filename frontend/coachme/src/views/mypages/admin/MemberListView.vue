629e9fa99d12c6fbe3cab4384f8212b43051d963
Sangyeop Nam <spearoad15@gmail.com>
2024-01-31 15:07:32 +0900

    fix : 회원 아이디 수정
    Former-commit-id: 25aecdfd70fbf0402d52401a4f9a11af5c7f44c7 [formerly 959d7740a2e90c67fe99c6d6573f0aec883e01d9]
    Former-commit-id: df1a92dca371f560dec1efec9cad72b46123a789

--- frontend/coachme/src/views/mypages/admin/MemberListView.vue
+++ frontend/coachme/src/views/mypages/admin/MemberListView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28475fd6405124efbd389411cd0f9586275a33bcb5916bdd9673cc2d1f72351d
-size 1466
+oid sha256:20029548ed136fe14d97e8ca00aaef7f3a40f0e4d0b3f8ec6baeb804f1f3042a
+size 1472
