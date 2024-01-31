83506026ff3230deb4c75351c4e91b3035b09521
Sangyeop Nam <spearoad15@gmail.com>
2024-01-31 12:30:41 +0900

    refact : controller response data 수정
    Former-commit-id: 799f9df59cace3570b0878a0139509999b294c28

--- backend/src/main/java/com/ssafy/api/admin/controller/AdminController.java
+++ backend/src/main/java/com/ssafy/api/admin/controller/AdminController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89187fefe5ec7dff4cea639544ea6ea018e21699167722d249395c5739c68fba
+oid sha256:b43523b97e1a668b3edc0f89833b88e91065a04f0d09b2f5594b89692e4bd1c4
 size 1778
