ad7ef412d913baeeb5214ea2f7bfc639404c99d5
Sangyeop Nam <spearoad15@gmail.com>
2024-01-31 09:01:46 +0900

    fix : 백엔드 서버 주소 변경
    Former-commit-id: 76b937c180d313b260748fc83ac0546504897c29

--- frontend/coachme/src/views/mypages/admin/ElevationListView.vue
+++ frontend/coachme/src/views/mypages/admin/ElevationListView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15e2e5f02c67b9182c5bec5cc82651c482dbd25f1aaed3426bb775d108383cc1
-size 3789
+oid sha256:4e920b9da4b0b443ed8fd95f49b11b6ce64fde0c2d09224e4390e025d0e8abe1
+size 3807
