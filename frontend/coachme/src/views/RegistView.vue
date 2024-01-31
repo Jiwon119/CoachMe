e98e92aa80dd86bc58b2193160d50fd872923426
Sangyeop Nam <spearoad15@gmail.com>
2024-01-31 11:40:04 +0900

    header test
    Former-commit-id: 3a17414a99de0932e7028c30167b29d046efca77

--- frontend/coachme/src/views/RegistView.vue
+++ frontend/coachme/src/views/RegistView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af3fab64ce2e6ba1f92b21ea9b0a5e60da71210b11a4aaaf2c07bf5eaa2a2673
-size 9298
+oid sha256:6084ddb471ca9d36f75456c8d5c34c6508284cf4317986d502bdd52152c3ff46
+size 9466
