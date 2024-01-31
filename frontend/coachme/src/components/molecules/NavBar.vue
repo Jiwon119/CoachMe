1c5cea0573348bf2c2096e4f8a02a779a40f1814
Sangyeop Nam <spearoad15@gmail.com>
2024-01-31 17:54:55 +0900

    refact : 로고 변경
    Former-commit-id: 247f8ba890697ee8cdcc496090a29c7e141b6049

--- frontend/coachme/src/components/molecules/NavBar.vue
+++ frontend/coachme/src/components/molecules/NavBar.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a0d501da1cb983e0b1572158def0f9bfaae0a1cd9608a8eca18e6098c04e70b
-size 795
+oid sha256:8dddea1f49cdf72cb27ee9a44e90ba3aada53cddd6b096aa89dab8f3cf0521b2
+size 811
