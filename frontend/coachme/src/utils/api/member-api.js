a31ff0fe78821d780de3fe4f634987e5f50d3391
Sangyeop Nam <spearoad15@gmail.com>
2024-01-31 11:40:04 +0900

    header test
    Former-commit-id: e98e92aa80dd86bc58b2193160d50fd872923426 [formerly 3a17414a99de0932e7028c30167b29d046efca77]
    Former-commit-id: 3363e9f88093a8d5a2d67306dfb9409e102a949c

--- frontend/coachme/src/utils/api/member-api.js
+++ frontend/coachme/src/utils/api/member-api.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b5a998b7873891711e845e103c7a23351a11d30c15a6c176f909dedeaef0a74
-size 8029
+oid sha256:78955e1e1769640f43a69911a0bee969839bad3625b8b4b73e34c27eec6c269e
+size 8077
