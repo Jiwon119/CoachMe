90f6e45592a157c7b992b45b7580e6d46a60d34d
Sangyeop Nam <spearoad15@gmail.com>
2024-01-31 16:28:19 +0900

    refact : 불필요 test파일 삭제
    Former-commit-id: 88b6cf75984a568d07b59cc31a0909f069ae8582 [formerly e3a1c6b3c718d3f66dfcc58ca17db3acec990c5d]
    Former-commit-id: ecabad621f035bd562df2bec4806076b670fa7b2

--- frontend/coachme/src/tests/components/TestProfileImage.vue
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:dcc97002f736cb138e8d15ef83fd2c15f36aa77490fdb3cace463f70d0956d69
-size 674
--- frontend/coachme/src/tests/views/TestIndexView.vue
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:686e24eba168346a13aeefc5fb09c2de4f226aca4ef3f47c57c8f933b8523171
-size 717
