e74cc3655a80cea155e33159d74c0875a4f3d2ae
Sangyeop Nam <spearoad15@gmail.com>
2024-01-31 16:31:13 +0900

    refact : 불필요 파일 추가삭제
    Former-commit-id: 1fee82367e480cb25273f10396b427797efec035

--- frontend/coachme/src/views/mypages/InterestView.vue
+++ frontend/coachme/src/views/mypages/InterestView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aabde4c15c60c7312bed9dd3abf33a96896db3f33333d5fee91c99cfb5390e3b
-size 4669
+oid sha256:d789fce655e08bc43b84b80fc2720864c8ec93c8efa43494a1c3f2f164eea392
+size 4608
--- frontend/coachme/src/views/mypages/admin/MemberListView.vue
+++ frontend/coachme/src/views/mypages/admin/MemberListView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20029548ed136fe14d97e8ca00aaef7f3a40f0e4d0b3f8ec6baeb804f1f3042a
-size 1472
+oid sha256:0ca5d08507be94a3abd0935541715187cabaee026ce2fc556c7178671f14876e
+size 1478
