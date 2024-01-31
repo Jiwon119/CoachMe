f92cfa0afcc503270cccec9af7a0c6bb39cc941f
Sangyeop Nam <spearoad15@gmail.com>
2024-01-31 16:31:13 +0900

    refact : 불필요 파일 추가삭제
    Former-commit-id: e74cc3655a80cea155e33159d74c0875a4f3d2ae [formerly 1fee82367e480cb25273f10396b427797efec035]
    Former-commit-id: 45611a4784b535670379a6da82cfe55c72231c11

--- frontend/coachme/src/views/mypages/coaching/CoameScheduleView.vue
+++ frontend/coachme/src/views/mypages/coaching/CoameScheduleView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a86a516ba8bea62f7210dad7753e6ec9361756a2efc5ef22f3875702caa7a0e5
-size 4167
+oid sha256:a5730ebf953b27b362c30d628b6fa04ff0cf027fa6c9f7adb980dc554a6f6cbe
+size 4093
