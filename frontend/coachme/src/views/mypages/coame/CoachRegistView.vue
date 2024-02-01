a8b9c337e0a0ece23be2cf68620a89c67af24a1f
김동현 <silverstuff@naver.com>
2024-02-01 10:43:57 +0900

    S10P12A403-468-f-desktop-5-코치-마이페이지-api-정리/feat/1
    Former-commit-id: 91f9146370a1ba3c7d992e8b8b9f7d448044bdbc

--- frontend/coachme/src/views/mypages/coame/CoachRegistView.vue
+++ frontend/coachme/src/views/mypages/coame/CoachRegistView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80a96fb533780796972379689c8aafa09743125277c598bb9f94ca5d3c6fd4a2
-size 2431
+oid sha256:55d8ed65d6b29ac1e6096512adb9fd3e642b97dcbf06c00c054a474a9e406427
+size 2435
