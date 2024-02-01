4b15527abf27fca0110a92ae902038a5eae059bc
SangyeopNam <spearoad15@gmail.com>
2024-02-01 23:37:46 +0900

    fix : 로그인 시 pinia 사용해서 정보 저장
    Former-commit-id: d8dd528a35d8b313930a9bc1dfab7248ad966c3f

--- frontend/coachme/src/views/mypages/MypageMainView.vue
+++ frontend/coachme/src/views/mypages/MypageMainView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9c2aea5baa6d7d65860dc7d7b82877356352f4dd6786dc6003810d9d145c8de
-size 1756
+oid sha256:76b042e6713df6a87191ee1477c568e952457a9780459010b3dce41a1eb2e884
+size 1798
