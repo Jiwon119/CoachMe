d61d47f6aee22f906bc601875c5c9852b565d739
Sangyeop Nam <spearoad15@gmail.com>
2024-02-01 14:21:14 +0900

    fix : 불필요 파일 삭제, 포트주소 변경
    Former-commit-id: 8256f8f07101cc78514095b0ae94b3464aeafd34

--- frontend/coachme/src/views/mypages/coame/CoachingScheduleView.vue
+++ frontend/coachme/src/views/mypages/coame/CoachingScheduleView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:886ae34ab7c8ba8c11bcc855020785ebec39609c86948bf0b4f51c44fb68f7f6
-size 4101
+oid sha256:a5730ebf953b27b362c30d628b6fa04ff0cf027fa6c9f7adb980dc554a6f6cbe
+size 4093
