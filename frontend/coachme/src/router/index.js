1c9037b7cabc27e88591a859d67f57c5dac00a0e
Sangyeop Nam <spearoad15@gmail.com>
2024-02-01 13:00:31 +0900

    gitignore 추가
    Former-commit-id: b66c3bc465b6c9d4c28d8d05decc3f8e2204308a [formerly 401a01e1bb3f11ae527944d65a0c2b5ee068c24f]
    Former-commit-id: 4bcf2f705c31d46cac6398ba64403d5fede21e62

--- frontend/coachme/src/router/index.js
+++ frontend/coachme/src/router/index.js
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f4d77ffd954cf6d5dfee7233a61824c6f65d3eb7d0790f440f25c6ec0aee305a
-size 431
+import { createRouter, createWebHistory } from 'vue-router'
+import mainIndex from './indexes/main-index'
+import mypageIndex from './indexes/mypage-index'
+import searchIndex from './indexes/search-index'
+import liveIndex from './indexes/live-index'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [...mainIndex, ...mypageIndex, ...searchIndex, ...liveIndex]
+})
+
+export default router
