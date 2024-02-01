ad17b0c66d1365196acc3a35c9cd679fb5f164ea
Sangyeop Nam <spearoad15@gmail.com>
2024-02-01 09:42:06 +0900

    docs : api 일부 수정
    Former-commit-id: 6687fb5f86f94e30847e083ad7d65f5b3483dbe5

--- docs/src/apispec/CoachingAPI.js
+++ docs/src/apispec/CoachingAPI.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:020d5df548504589b076a5e427538d55d8298e4bfac611c6d48b463c69b18e26
-size 5064
+oid sha256:aef02446ed9e5d05c687f0dd8b14c265f24ad9006a08f1162c80c7acbb519201
+size 5081
