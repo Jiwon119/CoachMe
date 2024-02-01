64cbd29aebfc7b6af390788c81e384e7f5eeb7b9
Sangyeop Nam <spearoad15@gmail.com>
2024-02-01 15:56:46 +0900

    fix : cookie - cors 수정 ``` configuration.setAllowCredentials(true); configuration.setAllowedOrigins(Arrays.asList("https://i10a403.ssafy.io", "http://localhost:5173")); ```
    Former-commit-id: 538098a688889304ca7616a7ac419c5a9e9665bf [formerly 3a46951b38b67505bf2dc6185406f89ebc686cae]
    Former-commit-id: 1d313c4bd6bf33fad49193cf35832830e189ad90

--- frontend/coachme/src/utils/api/auth-api.js
+++ frontend/coachme/src/utils/api/auth-api.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f83b8998d0997ced73df37909f8a3a6e638983fbffdb39b0df58a7b4a1052af5
-size 1564
+oid sha256:b0d71ca292e1dfb969ba90be64b9f11af2c08aed9ecc2e0c7d1173a123aafc61
+size 1622
