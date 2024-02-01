b69cf80074d2cfb09e02ee389beb2c0405c2bf91
Sangyeop Nam <spearoad15@gmail.com>
2024-02-01 17:10:34 +0900

    log
    Former-commit-id: 4fbaa5cd94fe3b57d8917493abcfe00c7cb00bc1 [formerly 45b0c44f3d00a00780ec6c1809c882bc7aeaad42]
    Former-commit-id: 57d51f3bfeaf184004eabef98576a6b4196fee9e

--- frontend/coachme/src/views/LoginView.vue
+++ frontend/coachme/src/views/LoginView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e9636945203f4aa67c455af1d580636a2e554538e2b75ec23ed000fdcc2bada
-size 4827
+oid sha256:9cee12c86e683d94697397202e5e0b6080666b8d7af242bee1ea686df0144bc8
+size 4830
