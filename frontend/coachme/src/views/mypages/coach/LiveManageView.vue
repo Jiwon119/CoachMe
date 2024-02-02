9be95337d2a78b823cf2f8cac4708e06d02255e5
김동현 <silverstuff@naver.com>
2024-02-02 10:17:16 +0900

    S10P12A403-364-f-desktop-5-2-코치관리-코치-포트폴리오-관리/feat/1 fix : css수정 및 버튼 수정
    Former-commit-id: cb2c0c7a3e2c84fa9d5a3567a76440e8c54e6153 [formerly 4cda161edffb4a3546c7173664ff56df9d9d4c2d]
    Former-commit-id: 45113f4b3ae42d29a6a1876da884ac405b87fe2a

--- frontend/coachme/src/views/mypages/coach/LiveManageView.vue
+++ frontend/coachme/src/views/mypages/coach/LiveManageView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68f45b208fdd3e670a50ecd6c0884fe4ba3f58ce5a4f263a024f15cdef6518e4
-size 5814
+oid sha256:99a608dd262cc04fd2f1d38375f79ceefdd4921bc325f50baaab356d61c28439
+size 5813
--- frontend/coachme/src/views/mypages/coach/PortfolioView.vue
+++ frontend/coachme/src/views/mypages/coach/PortfolioView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e305706818251aa3739f5f3388b49cca0a6d82b5239e3ab3953d9115728d837
-size 76
+oid sha256:2afa05f33dde182914bb1211afdec31cb841b5c5531b0cf42afd117ed52c0179
+size 2148
