f65197dd2966c23f2eec0cf1984b946fe5433a61
KDH <silverstuff@naver.com>
2024-02-02 10:11:37 +0900

    fix : 버튼수정 및 css 수정
    Former-commit-id: c4d9d459f7c233bc649b1d20ca11a06f7ea6e889

--- frontend/coachme/src/views/mypages/coach/CoachingManageView.vue
+++ frontend/coachme/src/views/mypages/coach/CoachingManageView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d45d98a5dda4e0578a5f87a130ed1719c3056419cec54ca9d3e6b74f01566ef
-size 4164
+oid sha256:226a371efe10081568e075b115b578e3ee21904931e43cdeba3dce4727905b4f
+size 4103
