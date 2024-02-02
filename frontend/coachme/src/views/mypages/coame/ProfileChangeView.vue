29651efb4d1deb63287484c7ee41c1c8a4ad64fd
KDH <silverstuff@naver.com>
2024-02-02 10:11:37 +0900

    fix : 버튼수정 및 css 수정
    Former-commit-id: f65197dd2966c23f2eec0cf1984b946fe5433a61 [formerly c4d9d459f7c233bc649b1d20ca11a06f7ea6e889]
    Former-commit-id: 00d2e0602fbe5e6b93533ff3d4084a154bacf303

--- frontend/coachme/src/views/mypages/coame/ProfileChangeView.vue
+++ frontend/coachme/src/views/mypages/coame/ProfileChangeView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74f5cbe855728b945aeb63b365e2f950cdbaac1686fb547526d063e16db751c0
-size 4527
+oid sha256:e10510adfb1aeb62439bbaee2cde1d53770949c35037870f09719ff10431b077
+size 4924
