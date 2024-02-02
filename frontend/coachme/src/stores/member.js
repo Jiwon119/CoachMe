9a0dfbb56ce422f4072327a52c7b34bdc0e8aa14
남상엽 <spearoad15@gmail.com>
2024-02-02 21:57:22 +0900

    S10P12A403-444-f-프로필-crud-api-구현/feat/2
    Former-commit-id: 9ac369346874f927a02c4281e7cf282a5a76e7a7 [formerly 3e4ea4aecfd65341cb9573f163fef331cc0d71a1]
    Former-commit-id: 3fa48c206bc224bd6f4edd4f20b2cae98fd194c3

--- frontend/coachme/src/stores/member.js
+++ frontend/coachme/src/stores/member.js
@@ -1,3 +1,10 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9098853cb7992da62ffee0ab61aaa21605241c40bb5cb814c118a88ec7f01c9e
-size 183
+import { defineStore } from 'pinia'
+
+export const useMemberStore = defineStore('member', {
+  state: () => {
+    return {
+      profileText: '프로필을 등록하세요.',
+      profileImageUrl: '/src/assets/icons/coame.png'
+    }
+  }
+})
