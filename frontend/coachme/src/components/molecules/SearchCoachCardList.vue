1e3da1b1cb0d8f66bb5de541d93bca37ed9f4184
이혜지 <lee02g29@naver.com>
2024-02-02 10:01:40 +0900

    S10P12A403-476-f-desktop-17-1-화상페이지-코치/feat/3 - 1
    Former-commit-id: 96860d90affd78a5c330771cc13fa4d6c0d3f386 [formerly 0c9988e8f88c9ba78c192d0766f1693b8b80d6d0]
    Former-commit-id: d396664b2cf6d8dc9db6f729b9bdd1088d7b473c

--- frontend/coachme/src/components/molecules/SearchCoachCardList.vue
+++ frontend/coachme/src/components/molecules/SearchCoachCardList.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:00a605aa9dee9e587d95f1d26c6db9e4b602ff71036e1d5f9abd4c6ef1f3ab0e
-size 10727
+oid sha256:ddd7d6bae6917e7777f62695922f54f38c89ed9534327e0a94b18046c7a5143a
+size 10019
