70adce9127e82d3e0ec25c89db8db2a4d46eed8d
이혜지 <lee02g29@naver.com>
2024-02-02 14:31:13 +0900

    feat : 코치 검색페이지 코치 리스트 추가, 코치 카드와 코치리스트 전환 기능 추가
    Former-commit-id: 02b0ddda7624f8b4e96935705b7883eda164d934

---
+++ frontend/coachme/src/components/molecules/SearchCoachList.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:41c4ac8936d8e579e0be3b680956ce74b8cf3984592daae07631faf3eeff0d92
+size 4038
--- frontend/coachme/src/components/molecules/SelectCoachCard.vue
+++ frontend/coachme/src/components/molecules/SelectCoachCard.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f5b27f9218c32e289ddfcaa2f6eb1f7b99e665026725ad47a5e484eb50b89975
-size 3166
+oid sha256:0ba797549461f68bf4ba28fc97193d1e4156baee62b7d6c6bd650b55336ad46e
+size 3130
