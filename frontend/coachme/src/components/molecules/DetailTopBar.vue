96860d90affd78a5c330771cc13fa4d6c0d3f386
이혜지 <lee02g29@naver.com>
2024-02-02 10:01:40 +0900

    S10P12A403-476-f-desktop-17-1-화상페이지-코치/feat/3 - 1
    Former-commit-id: 0c9988e8f88c9ba78c192d0766f1693b8b80d6d0

--- frontend/coachme/src/components/molecules/DetailTopBar.vue
+++ frontend/coachme/src/components/molecules/DetailTopBar.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b629743acc9491973e891630d0be69f662e1d600d42a27b4091af3b36484ec5
-size 890
+oid sha256:c323f445e7394d0d4cdb766e2181d5602dde29a70684d9abc95535b38dab9f59
+size 591
