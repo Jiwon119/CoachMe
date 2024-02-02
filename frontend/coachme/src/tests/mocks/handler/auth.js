9ac369346874f927a02c4281e7cf282a5a76e7a7
남상엽 <spearoad15@gmail.com>
2024-02-02 21:57:22 +0900

    S10P12A403-444-f-프로필-crud-api-구현/feat/2
    Former-commit-id: 3e4ea4aecfd65341cb9573f163fef331cc0d71a1

--- frontend/coachme/src/tests/mocks/handler/auth.js
+++ frontend/coachme/src/tests/mocks/handler/auth.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:052b0b5adc1e1c4037bbf0e375b7775a4ed63d32fb692bd2364c6407fa12bcf9
-size 2188
+oid sha256:2174641d2758ca06f1b748cbbf48d28957cc4ade89207f9afa6067c335ededb6
+size 2596
