cdaf483b4b2ebc1a68536f494341b8feb8660b0b
KDH <silverstuff@naver.com>
2024-02-02 10:19:45 +0900

    merge
    Former-commit-id: 49008478515a8ea0caac9e7b3bab52ad017cdb53

--- frontend/coachme/src/views/mypages/coach/LiveManageView.vue
+++ frontend/coachme/src/views/mypages/coach/LiveManageView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99a608dd262cc04fd2f1d38375f79ceefdd4921bc325f50baaab356d61c28439
-size 5813
+oid sha256:b30b6f4fcf58777aa87cda13a158613c56e45cd3c605df4cba19c95160c36cba
+size 5707
--- frontend/coachme/src/views/mypages/coame/CoachingScheduleView.vue
+++ frontend/coachme/src/views/mypages/coame/CoachingScheduleView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4adb4d181477846ee80a6cff37cfa49ac9f8ea543769e1d3117f61ddda6296de
-size 4176
+oid sha256:ed2206606ca5116406642a3a15d32ef1c65a6ecd1f67f8282935ff7e691c7374
+size 4070
