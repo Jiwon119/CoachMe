cb2c0c7a3e2c84fa9d5a3567a76440e8c54e6153
김동현 <silverstuff@naver.com>
2024-02-02 10:17:16 +0900

    S10P12A403-364-f-desktop-5-2-코치관리-코치-포트폴리오-관리/feat/1 fix : css수정 및 버튼 수정
    Former-commit-id: 4cda161edffb4a3546c7173664ff56df9d9d4c2d

--- frontend/coachme/src/views/mypages/coach/PortfolioView.vue
+++ frontend/coachme/src/views/mypages/coach/PortfolioView.vue
@@ -1,4 +1,3 @@
-<script setup></script>
-<template>포트</template>
-
-<style scoped></style>
+version https://git-lfs.github.com/spec/v1
+oid sha256:2afa05f33dde182914bb1211afdec31cb841b5c5531b0cf42afd117ed52c0179
+size 2148
