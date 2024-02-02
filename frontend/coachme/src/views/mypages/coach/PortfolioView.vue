98910928e4bfa01fd9eaaa76a884c458f06a01f1
KDH <silverstuff@naver.com>
2024-02-02 10:19:45 +0900

    merge
    Former-commit-id: cdaf483b4b2ebc1a68536f494341b8feb8660b0b [formerly 49008478515a8ea0caac9e7b3bab52ad017cdb53]
    Former-commit-id: c875649e6c25f093cc68e71badfd967544ffbd2f

--- frontend/coachme/src/views/mypages/coach/PortfolioView.vue
+++ frontend/coachme/src/views/mypages/coach/PortfolioView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2afa05f33dde182914bb1211afdec31cb841b5c5531b0cf42afd117ed52c0179
-size 2148
+oid sha256:f7c17c09e5f356d567a8bade4622f86881643684bf11d5040b082d21da718394
+size 2042
