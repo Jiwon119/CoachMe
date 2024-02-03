d0aae83bee327453b2122c16446812c9ae480229
SangyeopNam <spearoad15@gmail.com>
2024-02-04 00:13:06 +0900

    fix : 파일 삭제 시 파일 엔티티 삭제 후 기본파일 경로를 가진 엔티티를 매핑하는 것으로 로직 수정
    Former-commit-id: 498f4edf6c1d6bf0144125d51cd649a3d8dd7827 [formerly 79ba91a65c3fca738bc8e3a6c87fde092e39ec83]
    Former-commit-id: d0745ff51c8177de38c4b7d0bf661117cc33209a

--- backend/src/main/java/com/ssafy/db/entity/Member.java
+++ backend/src/main/java/com/ssafy/db/entity/Member.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb4a8e2f4db1b04a8ab7df5aa8d5d507f3a55519ce73617a0d7adf61d2990d56
-size 4521
+oid sha256:abc8ae22ec8cbebc90eb4480f682bf5a2d838850eb8327d169f1f50acbe5b61a
+size 4649
