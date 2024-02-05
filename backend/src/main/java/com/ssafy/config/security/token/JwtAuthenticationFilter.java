b81545955031d728f8684325d2c63fd41c2d2eae
이준학 <0710june@naver.com>
2024-02-05 18:23:08 +0900

    fix: 불필요 파일 삭제
    Former-commit-id: fd08a5f589091d4a006cfbdb337aa2eb680df2ee

--- backend/src/main/java/com/ssafy/config/security/token/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/ssafy/config/security/token/JwtAuthenticationFilter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2f9d98cc3929b1c6f45f6befea3b08164319c80b1954aa898c4cd8a5975df11
-size 5224
+oid sha256:d2238f263580b0771bf69e7b3d3e4835e8a074901bde38c2a7b4be9f6edac054
+size 5236
