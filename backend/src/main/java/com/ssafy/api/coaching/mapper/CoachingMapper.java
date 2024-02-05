8c5a4d1aa66572891d29ce614d7ad856f54370fa
Sangyeop Nam <spearoad15@gmail.com>
2024-02-05 18:02:24 +0900

    fix : 코칭 생성기능 오류 수정
    Former-commit-id: f2fc8755cc26885e79c770bb4fd324d1c7711c6d [formerly e406e4ff777541d5992a095930b96ca1578bde0d]
    Former-commit-id: c5aaeb7735de30a9eef07f6fba79f3155fa36be5

--- backend/src/main/java/com/ssafy/api/coaching/mapper/CoachingMapper.java
+++ backend/src/main/java/com/ssafy/api/coaching/mapper/CoachingMapper.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f68b54ef4c7391a7bca07a7c6bf6e5477270ddf78888eed79c1293ec7df93af8
-size 2609
+oid sha256:a81b98b9c23ac310b1fd5a956304764a9988fa00ec4c2eb13d9023a2b631bf09
+size 2488
