be9d6909898e76fd614a410c8c1fbf4b2025c1b4
Sangyeop Nam <spearoad15@gmail.com>
2024-02-05 14:42:28 +0900

    fix : 이미지파일 절대경로 수정(assets)
    Former-commit-id: 3034e48b4e310a0bbe5a17383c0a20ab5a2442e9 [formerly 11eb4166777c4c9695ebe710825d2da0d74bc234]
    Former-commit-id: e574b2a263a033ade38793f12edcd5873e1afebf

--- frontend/coachme/src/stores/member.js
+++ frontend/coachme/src/stores/member.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62403a5119b3e1e1edabf0d56fdf51fed93140292e47e386e9e8088f5e88b78f
-size 399
+oid sha256:47de562e08ce67a857d229eee8de0e28ab9a6887c585d46f05d26220e81505c4
+size 395
