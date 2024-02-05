dbdddf74f5fac85c6b31b220486250990dbedc4e
김지원 <wldnjs35242@naver.com>
2024-02-05 16:45:32 +0900

    feat : 코칭 상세페이지 조회 API 구현, 카테고리 코치 리스트 검색 api 수정
    Former-commit-id: a5191f9939b18cfa32e82ec1d8a9f646287801bf

--- backend/src/main/java/com/ssafy/api/coach/service/CoachService.java
+++ backend/src/main/java/com/ssafy/api/coach/service/CoachService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:544b68cfe572f01a3e71478c501eb0aaed53b0cda28a88dbad2a2158b3197cc6
-size 2141
+oid sha256:178b458bbdd491f19a7468295c264c652f09a6db9ac4f87f5b9eac2fe0d2703b
+size 2305
--- backend/src/main/java/com/ssafy/api/coaching/mapper/CoachingMapper.java
+++ backend/src/main/java/com/ssafy/api/coaching/mapper/CoachingMapper.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e576c057118f47bda1845bba645f7bf60981252d8ebfc501b8543946d4b9d41
-size 1341
+oid sha256:b8147afbed932b16b72de26d9793bf48f2c9de70429adae304649a4791c8d0b2
+size 1684
