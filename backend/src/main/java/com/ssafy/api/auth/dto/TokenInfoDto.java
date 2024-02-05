b5e6aae2439946c4126c1bb9d13f748d07737dc7
Sangyeop Nam <spearoad15@gmail.com>
2024-02-05 15:02:32 +0900

    fix : Merge하면서 충돌난 오류 해결
    Former-commit-id: 97bda7d5469d750193c75bfbb56034a8ff10ee8c [formerly 72848cd051df5d42aa8a0594b7362c589451fdb3]
    Former-commit-id: 9d4e22aa611fc5e8aec983d8e0f77bc9907e15c3

---
+++ backend/src/main/java/com/ssafy/api/auth/dto/TokenInfoDto.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:eff1e162625186a5479bb9a6014f1c4982e2942e3b3191568343947649e7a221
+size 234
--- backend/src/main/java/com/ssafy/api/coach/dto/request/CoachesRequestDto.java
+++ backend/src/main/java/com/ssafy/api/coach/dto/request/CoachesRequestDto.java
@@ -1,3 +1,9 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f4e5b85b6c50c26d6240bfaef96765431c96a9dc3c38057bae1354ca1f089f3c
-size 144
+package com.ssafy.api.coach.dto.request;
+
+import lombok.Data;
+
+@Data
+public class CoachesRequestDto {
+  String division1;
+  String division2;
+}
