6174a8c8b727802d0f6e1f8169ecd6c51f5e107d
Sangyeop Nam <spearoad15@gmail.com>
2024-02-05 14:23:55 +0900

    fix : merge 중 오류 수정
    Former-commit-id: 2e519782fc1662d482052fc6571d8d9d536da192 [formerly 24753c26a089833965da807531e5f857ac518c64]
    Former-commit-id: ae70340c84f97a776f5e41c2af14b365754cafb2

--- frontend/coachme/src/tests/mocks/worker.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e16cecd2b58911f9bbc4be9c944adc6691e3830eefe39391fc4dfc5d7ba61bb7
-size 257
--- frontend/coachme/src/views/mypages/coame/CoachingScheduleView.vue
+++ frontend/coachme/src/views/mypages/coame/CoachingScheduleView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b7f63c3d46734d9153fdfee98872e047ac4739b5a63efb68672f73e958d576b
-size 3996
+oid sha256:be32e959ed8b2c14d3f5d2f4f2e0672d1539a1ef2c005be0e68b3c5d77829c94
+size 3428
