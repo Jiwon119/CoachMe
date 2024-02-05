e2a974a0c0aa116ae44cb4618c20ee325199ddd0
SangyeopNam <spearoad15@gmail.com>
2024-02-05 21:11:09 +0900

    fix : 회원정보 수정 로직 변경
    Former-commit-id: a90bf83d80e197ea8b970fedc0728ad53388bda6 [formerly 920cb242cad4ac108abe25e697bfccbd43ad5d19]
    Former-commit-id: ee9d902da34ef04f5f9ad6f2de1a1924f07f52d4

--- frontend/coachme/src/views/mypages/coame/ProfileChangeView.vue
+++ frontend/coachme/src/views/mypages/coame/ProfileChangeView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79e5644dd86f5736821b8daef65b55d5f43a3f22d7c023c00e82d0c9b7d82eb5
-size 9025
+oid sha256:d18e2423679ee14eea7fe4d4891cdbf2de33a43f14377b9247e83fef9a80f5b6
+size 9215
