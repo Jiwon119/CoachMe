06a01860847a8729eeb24753e47e2cce4cbc020d
이혜지 <lee02g29@naver.com>
2024-02-05 10:15:51 +0900

    S10P12A403-476-f-desktop-17-1-화상페이지-코치/feat/3 - 2
    Former-commit-id: 239d3f3600b35844f5464b34484ed8dbe466bb59 [formerly 4827e75107ce47d1c0988f40b434569a361bf874]
    Former-commit-id: 9dcbc50f9fd6744670f6b8a8da970e7c3ff2ab42

--- frontend/coachme/src/views/live/LiveCoame.vue
+++ frontend/coachme/src/views/live/LiveCoame.vue
@@ -1,4 +1,3 @@
-<script setup></script>
-<template>
-  <h1>Live Coaching on coame page</h1>
-</template>
+version https://git-lfs.github.com/spec/v1
+oid sha256:d9948da16ee08d5033a0b8b2fc14f38d8410300f97df0a98db424a71556a38a2
+size 870
--- frontend/coachme/src/views/search/coaching/CoachingList.vue
+++ frontend/coachme/src/views/search/coaching/CoachingList.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be04c296ee32ae75a33a00af08bc045062dd9de869a6ce6502438053a9002148
-size 4278
+oid sha256:9f21e0270dbf74600e12b321fa805364b7c8641eb9eba1489aac42a0e7c26015
+size 4522
