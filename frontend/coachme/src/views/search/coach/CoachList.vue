184560610df05136729a0a186b2b208b0d6acad3
이혜지 <lee02g29@naver.com>
2024-02-05 10:00:58 +0900

    S10P12A403-476 save : 진행 상황 저장
    Former-commit-id: 313ac841a3d058d4f72bedc35875ebdfb3c259f6

--- frontend/coachme/src/views/search/coach/CoachList.vue
+++ frontend/coachme/src/views/search/coach/CoachList.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7c8b4a80531c3bd537e2831da713d9fba01c42b3020486cbcd9b35656f0b7f4
-size 5364
+oid sha256:24c3c119eea00884281d00b20c45ea1ec6bb5e3a348bd0f8fc809445fdc3f599
+size 5701
