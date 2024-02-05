6952d5e3850e949062702c4459a411b7e1de4100
이준학 <0710june@naver.com>
2024-02-05 18:10:17 +0900

    fix: 파일 위치 수정
    Former-commit-id: a831ac0514af2e08e9e578108bf8bba4ff38272f

--- backend/src/main/java/com/ssafy/api/coach/controller/CoachController.java
+++ backend/src/main/java/com/ssafy/api/coach/controller/CoachController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:450cff2e6cf6d2aafeea2ec970df0bbc9f33cde7714074a3e204eb6e2b151de9
-size 5829
+oid sha256:2f9ec6473afd9e10ab7a69de4857ab1c0f799250803cedc5cf969f9efb080437
+size 5892
