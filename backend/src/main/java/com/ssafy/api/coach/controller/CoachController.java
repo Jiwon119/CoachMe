f2fc8755cc26885e79c770bb4fd324d1c7711c6d
Sangyeop Nam <spearoad15@gmail.com>
2024-02-05 18:02:24 +0900

    fix : 코칭 생성기능 오류 수정
    Former-commit-id: e406e4ff777541d5992a095930b96ca1578bde0d

--- backend/src/main/java/com/ssafy/api/coach/controller/CoachController.java
+++ backend/src/main/java/com/ssafy/api/coach/controller/CoachController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b277eb3443e72594607b1686bb1250b97d82c6104317aa3e066cce1cfca566ca
-size 6001
+oid sha256:1ffd5cc12b9b612cd5aab6c4bab32f82b4d86f416bcc3fb47dc2e8da1ec13c94
+size 6017
