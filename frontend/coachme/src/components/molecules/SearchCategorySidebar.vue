ae7aec828ceaa9120694cd2ca821a464d0035e06
남상엽 <spearoad15@gmail.com>
2024-02-06 11:16:13 +0900

    S10P12A403-375-명세-필수-코치-coach-는-코칭-coaching-을-조회-및-수정할-수-있어야-한다/feat/2
    Former-commit-id: c146c5fca2b1c3843bedd25499a34537dee1c5b8 [formerly bf153c4b62e12d15bada0747eb0b6b74ed44b01a]
    Former-commit-id: 96e7aa758852268795e4f76c9e4dda6593dd1b11

--- frontend/coachme/src/components/molecules/SearchCategorySidebar.vue
+++ frontend/coachme/src/components/molecules/SearchCategorySidebar.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cab63a60250bd72bb1d7aaba231eefef6d4dd40ce940e73164cef2edc3c29ad0
-size 1116
+oid sha256:56cc7c3270938542793fc01583db6e293e53a64e8a91e87bf9724ec4d1094afb
+size 1310
--- frontend/coachme/src/components/molecules/SearchCoachList.vue
+++ frontend/coachme/src/components/molecules/SearchCoachList.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9ac3e65c12ac94991752323ded95a1d5fa938fedbd7e4151a630d0865d9f592
-size 4051
+oid sha256:6b57e70242a34f1ef37c190284dce52172ca672590c8f0594ad132c3c6fa059d
+size 4123
