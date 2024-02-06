5f42d4bbef93218f768aa6ae70ad1f00e598594a
Jiwon119 <92111247+Jiwon119@users.noreply.github.com>
2024-02-07 08:33:10 +0900

    웹소켓 연동 테스트
    Former-commit-id: 02abdcb044c15fd4342975663e3384c947504761 [formerly 6523516840f20e22b128f6e0b127ba35a6a8a9d8]
    Former-commit-id: 614c2d7de2804e6ddd1e174384e7a5f5c662b584

---
+++ backend/src/main/java/com/ssafy/config/StompExceptionHandler.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2fa6a91b34921b537636f455bb805198ef8bc93d7cf82785ae049e2dbd057fba
+size 4006
---
+++ backend/src/main/java/com/ssafy/config/WebSocketConfig.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fc1d1876bc5c013113b490247c78006e5e3fc610e01b30df7c45d16d8d56f1d7
+size 1334
