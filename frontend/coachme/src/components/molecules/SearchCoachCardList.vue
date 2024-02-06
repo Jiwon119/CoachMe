9aaf14113db3606bb02985b305616d0c35090b77
Sangyeop Nam <spearoad15@gmail.com>
2024-02-06 09:27:45 +0900

    fix : 카드 조회 시 받아온 코치가 0명일 때 오류 발생하는 부분 수정
    Former-commit-id: 40d49d2518d0ceb44b2725f27942f65d7b05ec43

--- frontend/coachme/src/components/molecules/SearchCoachCardList.vue
+++ frontend/coachme/src/components/molecules/SearchCoachCardList.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f71fe5df19c31d11c765a8b4ccd1df810933cd97c5c9f8ad6c92c3c8b1c11be
-size 9291
+oid sha256:f69c79a8be51416b1d4b9ce34bb42d8e8cec9d8aa38042b12f18cc3d2a11909e
+size 10065
