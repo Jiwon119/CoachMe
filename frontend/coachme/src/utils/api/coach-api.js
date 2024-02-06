3c8b8a565bd4571bde8189421951f48226995330
SangyeopNam <spearoad15@gmail.com>
2024-02-07 00:21:37 +0900

    feat : 리프레쉬 토큰 자동 갱신 - 이제 사용자는 리프레쉬 토큰이 있으면 별도의 조작 없이 엑세스토큰을 갱신할 수 있음
    Former-commit-id: de54ad81e33c48b0a393aefbc72f21d37caf0d50

--- frontend/coachme/src/utils/api/coach-api.js
+++ frontend/coachme/src/utils/api/coach-api.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f6626d335d0cf8c40816330b9cade1f9f671a98939368a11a030bd83030a6863
-size 3897
+oid sha256:6e86273bdf1e013435af3df7af143573b0cc0fdf29b5a0f21799398fc704e714
+size 3874
