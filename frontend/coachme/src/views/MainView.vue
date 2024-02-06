4eb8bf75cdafefe9aaa4cc78691755da0bb65733
이혜지 <lee02g29@naver.com>
2024-02-06 13:41:27 +0900

    S10P12A403-495 feat : 코미 화상페이지 기능 추가 코치 및 코미 화상페이지 버튼 동적 기능 추가
    Former-commit-id: 3af8209726e917b84d6ff56bfb19a441fba13ad0

--- frontend/coachme/src/views/MainView.vue
+++ frontend/coachme/src/views/MainView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:def8c832f4fa410eb410a5792886d52a433e75d6d3b68b45e9288c42f036d06c
+oid sha256:621c6d9b7646ea182eb6df93f462783b903b14a817fa6b4a523495235a0ca1ae
 size 8372
