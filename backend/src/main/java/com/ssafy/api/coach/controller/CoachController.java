7da1a16595a1de55d78cce0bf9195505378e6d9a
이준학 <0710june@naver.com>
2024-02-06 17:08:48 +0900

    fix: 에러 수정
    Former-commit-id: 3e48f350a5bb606500d18cd90b37eceb61bd58bc

--- backend/src/main/java/com/ssafy/api/coach/controller/CoachController.java
+++ backend/src/main/java/com/ssafy/api/coach/controller/CoachController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:059efa7e4b2bfac39fd22436210770d0c64eb388f9618b1761fd1b9f73c10425
-size 6553
+oid sha256:aceef238d1afb5a499ead83b595d04a6850e61edba19d8155497c6d42bed2da0
+size 6556
