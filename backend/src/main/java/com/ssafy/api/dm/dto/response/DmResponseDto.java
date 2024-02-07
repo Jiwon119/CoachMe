13421883b00b8b2f0c5fb90f29e59a74ddc5b003
김지원 <wldnjs35242@naver.com>
2024-02-07 17:29:15 +0900

    feat: DmResponseDto-수정
    Former-commit-id: 87a62c8d5783ba20a4fcedcd1a2a3f1a4baf14ea [formerly 23452a13ee7f11d310c0e006fdd526aad23e6988]
    Former-commit-id: 5341ea4a111299d80da3e9e41a12da8a4b3cd52b

--- backend/src/main/java/com/ssafy/api/dm/dto/response/DmResponseDto.java
+++ backend/src/main/java/com/ssafy/api/dm/dto/response/DmResponseDto.java
@@ -1,18 +1,3 @@
-package com.ssafy.api.dm.dto.response;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
-
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class DmResponseDto {
-  private long member;
-  private String message;
-  private LocalDateTime createDate;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:2b31c6c162a890d19544c6fc6ef22a839a086102cc8ac5b537112ee8d4cb0993
+size 350
