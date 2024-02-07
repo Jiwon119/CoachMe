43c503cc6e163f26e57635ba355ea0dc5d06da78
이혜지 <lee02g29@naver.com>
2024-02-07 15:28:39 +0900

    S10P12A403-498 fix : 미리보기 영상 없을 때 경우 추가
    Former-commit-id: f9cc10a7f07e257dd2ad77635674aec849f2747c

--- frontend/coachme/src/views/search/coach/CoachList.vue
+++ frontend/coachme/src/views/search/coach/CoachList.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b3fcdfdbe21e12d58907be4c27a967e5ba35e30a2cb5df41e3254ae1ce3090b
-size 7195
+oid sha256:9c8f46d4598979f46998e360d0c67ec0170aab07e374f33f34c97685328dc4fe
+size 7245
