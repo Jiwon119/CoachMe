1d75e00f9dea97335d5e437984e24a335a499c2a
김동현 <silverstuff@naver.com>
2024-02-07 09:34:48 +0900

    front-merge/dh
    Former-commit-id: e6cacd412776cad553e6f7525b7d82733bb95665 [formerly eec1ca3785e14530e060b1885db2135d6acb17eb]
    Former-commit-id: 1781d18edc73ba7c7e3214397cdfd2d689df2875

--- frontend/coachme/src/components/atoms/CustomCarousel.vue
+++ frontend/coachme/src/components/atoms/CustomCarousel.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:080398ca4550b069652d589efc310b8cafbc355c0b41f05cb967cd4048bd2eb7
-size 1590
+oid sha256:132d910adc663187db590934f7daadaee583997cfcb6f630d85ae60d924d6b37
+size 1641
