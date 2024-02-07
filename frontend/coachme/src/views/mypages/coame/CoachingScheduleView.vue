844d0499d6e4ca06731ab25c2e38437d4ed9a57b
Sangyeop Nam <spearoad15@gmail.com>
2024-02-07 09:53:01 +0900

    fix : merge conflict resolve
    Former-commit-id: 790dbc3dd45e9f0a9bf48d7bf1b18a53c3ec95f2

--- frontend/coachme/src/views/mypages/coame/CoachingScheduleView.vue
+++ frontend/coachme/src/views/mypages/coame/CoachingScheduleView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89a751a2659e9841dd59593707ecce275c1df63f6364ab6585f8a3ee1e8e05b3
-size 3043
+oid sha256:7d20cbb3b0a5f8ffea964221decf9fb9968e14ca774ab95a520afe0b24398e8e
+size 3052
