58d45eb3e6a5a80df633eef679dfbb85b7cf6de9
Sangyeop Nam <spearoad15@gmail.com>
2024-02-07 09:55:06 +0900

    fix : useCounterStore -> useDomainStore
    Former-commit-id: f80c2ecbe67cb9812fdffe80ad9635d6f5e7a4c7

--- frontend/coachme/src/components/molecules/SearchCoachingList.vue
+++ frontend/coachme/src/components/molecules/SearchCoachingList.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b685fed6c3d4acf50df68332cdc82454c0677de97a4997047da3adf6df92b69c
-size 4230
+oid sha256:0dab1b118c93ebaa9f86c742f2d3495e9b313ff850654289fe849bbb5374a924
+size 4361
