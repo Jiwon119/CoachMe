2055b83c42ecd84041ef1dd1d0e0ac23697e2700
이혜지 <lee02g29@naver.com>
2024-02-07 16:37:54 +0900

    S10P12A403-498 fix : 코치리스트 페이지네이션 버그 수정
    Former-commit-id: e6f48a91032afc08a1795c47b43aa88f79c6794b

--- frontend/coachme/src/components/molecules/SearchCoachList.vue
+++ frontend/coachme/src/components/molecules/SearchCoachList.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02cd882200be5e46694ab99837bb62b0c054a2928476db22798832352aa04ec7
-size 3999
+oid sha256:fbf4cb10474a8f352cbb2abb5a9f7afc6d371ba6d8ab754c6ad70886c4a3861d
+size 4004
