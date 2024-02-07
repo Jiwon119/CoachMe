daeb80218021b06017555a9b5e418d0f50b52b56
이혜지 <lee02g29@naver.com>
2024-02-07 16:37:54 +0900

    S10P12A403-498 fix : 코치리스트 페이지네이션 버그 수정
    Former-commit-id: 2055b83c42ecd84041ef1dd1d0e0ac23697e2700 [formerly e6f48a91032afc08a1795c47b43aa88f79c6794b]
    Former-commit-id: 4b3c0a56b504273c084b379df1bb303950c24edc

--- frontend/coachme/src/views/search/coach/CoachList.vue
+++ frontend/coachme/src/views/search/coach/CoachList.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c8f46d4598979f46998e360d0c67ec0170aab07e374f33f34c97685328dc4fe
-size 7245
+oid sha256:0e0cfa2b61f5a5958cc07c610f5b217a9fb97f4912396faf0bb5ddb20d1a0bb1
+size 7253
