348484021cbf0fd02e87bcc96d2d060179a8e008
Sangyeop Nam <spearoad15@gmail.com>
2024-02-07 13:30:35 +0900

    fix : 코치 리스트
    Former-commit-id: 48701a9966cb925713faebd78a24684478707b31 [formerly 1d9be74945549a4cc695b4e7d32171aba42fd03e]
    Former-commit-id: d680bf6d51aca2df3e4fb046a293286dc1088d3c

--- frontend/coachme/src/views/search/coach/CoachList.vue
+++ frontend/coachme/src/views/search/coach/CoachList.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b3fcdfdbe21e12d58907be4c27a967e5ba35e30a2cb5df41e3254ae1ce3090b
-size 7195
+oid sha256:769ce2941128fb34b44bdde89cfb1e3bec7b02c15dba322ea7db9541960f9d6e
+size 7736
