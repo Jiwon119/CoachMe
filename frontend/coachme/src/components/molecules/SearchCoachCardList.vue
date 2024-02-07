f1c027d45a0749be1cddccee42f3e0947cafc66c
이혜지 <lee02g29@naver.com>
2024-02-07 09:45:23 +0900

    S10P12A403-495 feat : 진실의 방 요청 버튼 레이아웃 변경
    Former-commit-id: 62394471420351e521f905f82fd509dd87f84cd0 [formerly 772589a4f36677c8b03359c188232a44a28c9f51]
    Former-commit-id: a55e1bb44a2073f1bf7fd3a988dfca55473d62ce

--- frontend/coachme/src/components/molecules/SearchCoachCardList.vue
+++ frontend/coachme/src/components/molecules/SearchCoachCardList.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2afcce53582ebdb9d998c33759e8d91d00dc1aec9e3cf705571ea4ac76e77caf
+oid sha256:33bfa678ea04fcb39d5af8f22a324c91f1a820f702578dd27c2420f08b972dfd
 size 9904
