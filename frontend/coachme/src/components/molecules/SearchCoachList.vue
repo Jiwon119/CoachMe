2d81308c050dedbcefb344f441a970c8c08f8aa7
Sangyeop Nam <spearoad15@gmail.com>
2024-02-07 09:48:50 +0900

    fix : merge conflict
    Former-commit-id: 11db05ce7d111431ccb041debc68d85077a7b488 [formerly a9208465354da369866dfee845f710af1a27695c]
    Former-commit-id: 2a82438d95110bbe45aee4b917a4fc1f8efc38bb

--- frontend/coachme/src/components/molecules/SearchCoachList.vue
+++ frontend/coachme/src/components/molecules/SearchCoachList.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c8f1fcafc828aa548116d6474ac8482e8b055b82eb671837d5553137606f8a6
-size 4303
+oid sha256:6e295ba6cf23395d4c953c0f1ca95e68b54ebb7a49b8bc0ea60b36d2ebd50b7c
+size 4065
