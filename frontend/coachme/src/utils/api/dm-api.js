1e0dac8d2970c653c033c35c529784a30d7f5890
Sangyeop Nam <spearoad15@gmail.com>
2024-02-07 17:59:12 +0900

    feat : dm 목록 조회 시 pinia에 해당정보 저장
    Former-commit-id: 5065d4b14def51a7f5f73c49b955c2b45a1715a1 [formerly 4691ea9509b300b0bfa197e4fceae0f4c4c6f93d]
    Former-commit-id: d2d8b0717ffa0ef015111f0ac545ee0853541c33

--- frontend/coachme/src/utils/api/dm-api.js
+++ frontend/coachme/src/utils/api/dm-api.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:241b48f90591b9f08f82301dcd9606527fcc569ff3074c3b860a03732fa81912
-size 1554
+oid sha256:2925134b59bb12b24e6fc290197ce6b8bfc235125ea790b2e0d18c8cf00f96ce
+size 1453
