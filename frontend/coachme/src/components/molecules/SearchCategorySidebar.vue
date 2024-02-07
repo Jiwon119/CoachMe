7612eac699141462c62d91753b9f6c9f67984f9f
이혜지 <lee02g29@naver.com>
2024-02-07 10:21:35 +0900

    S10P12A403-495-f-desktop-17-2-화상페이지-코미/feat/3
    Former-commit-id: d2be07164d646c5d2467e095ca8e1c42f8eeedb8 [formerly 77b130aaeecc452bff88d82632c217c7257eea05]
    Former-commit-id: 6be33fc8350c75fa397cb0f6aa7d6c2f3027ac1f

--- frontend/coachme/src/components/molecules/SearchCategorySidebar.vue
+++ frontend/coachme/src/components/molecules/SearchCategorySidebar.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:56cc7c3270938542793fc01583db6e293e53a64e8a91e87bf9724ec4d1094afb
-size 1310
+oid sha256:faac336de3f5253e21cc4d8c6302eb66dac4b16d4f7e460b34684b72812fbebf
+size 1309
