885ec61ba6cda86e7e72e5f27e5d60564c9cb1a8
Sangyeop Nam <spearoad15@gmail.com>
2024-02-07 09:55:06 +0900

    fix : useCounterStore -> useDomainStore
    Former-commit-id: 58d45eb3e6a5a80df633eef679dfbb85b7cf6de9 [formerly f80c2ecbe67cb9812fdffe80ad9635d6f5e7a4c7]
    Former-commit-id: 9c8503759da409c568c38de025dda3969410af06

--- frontend/coachme/src/stores/like-counter.js
+++ frontend/coachme/src/stores/like-counter.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc170068fee1514bfacc25175dbabbfec3bd63cbede9778493f30f0e1232e17b
-size 446
+oid sha256:c96f2fc7a4df747404ff351c19633a7c7e9661f17f49511c1cbe4107d82985d5
+size 438
