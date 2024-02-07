0a024ffed90ecea6f689778ea1a230de10255fc6
Sangyeop Nam <spearoad15@gmail.com>
2024-02-07 09:52:28 +0900

    fix
    Former-commit-id: bd333147ea756e55570d4dfea9bd5fd0a597bd03 [formerly 17e899e96e82a15ba18a230c4ee186e8a7de0092]
    Former-commit-id: ae62945ead141d594c7b298492cb7b6af7735334

--- frontend/coachme/src/views/mypages/coame/CoachingScheduleView.vue
+++ frontend/coachme/src/views/mypages/coame/CoachingScheduleView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:126f51743dee990614cfb32f9a9396a3ae078c6460783e534eadf7a870762e43
-size 3636
+oid sha256:7d20cbb3b0a5f8ffea964221decf9fb9968e14ca774ab95a520afe0b24398e8e
+size 3052
