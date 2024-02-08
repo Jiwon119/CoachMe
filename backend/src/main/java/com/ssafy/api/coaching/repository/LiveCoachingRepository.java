b8f6cc1539e02363951580f911ed7de51c8e97c0
이준학 <0710june@naver.com>
2024-02-08 14:57:52 +0900

    S10P12A403-491 feat: 영상 목록 조회, 대표 영상 설정, 라이브 코칭 생성 API 구현
    Former-commit-id: a461d01b4c7a2bf592491e0869d922cdc6870204 [formerly 9f87921577e06f70c5ac62bc0c8822a76dee91c2]
    Former-commit-id: e0259a82458c1d9b9742b4e4732a82efd646e664

--- backend/src/main/java/com/ssafy/api/coaching/repository/LiveCoachingRepository.java
+++ backend/src/main/java/com/ssafy/api/coaching/repository/LiveCoachingRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:378c2d88e2a4ff408ae96819331f93bc76794eb8d52825f5bdbb26eddad36d5d
-size 534
+oid sha256:ab34325360be999c922f83525f33b8b0df15db56e3f03bbe04bc7a7a07a91d06
+size 535
--- backend/src/main/java/com/ssafy/util/file/repository/FileRepository.java
+++ backend/src/main/java/com/ssafy/util/file/repository/FileRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f3bc577a901f2d5ad1817e80d5b177dbab9c57244395cce1b1a2daf9b765edd4
-size 271
+oid sha256:4b84855bfc380d556de1111defd037b65394e36b547514a71b75c53e86646a11
+size 511
