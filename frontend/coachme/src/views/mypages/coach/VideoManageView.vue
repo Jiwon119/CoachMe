ca028d8b84d04efc608892f70364a2b30e40406c
이혜지 <lee02g29@naver.com>
2024-02-08 10:23:44 +0900

    feat : 코칭 제목 별 영상 리스트 기능 추가
    Former-commit-id: c712541755e4dfa39a6625389525616422202eda [formerly 1990557279b3e0445d8e83c4b021da3f878fbe92]
    Former-commit-id: ea8224274475771121e80105c5d451e5f33aae0b

--- frontend/coachme/src/views/mypages/coach/VideoManageView.vue
+++ frontend/coachme/src/views/mypages/coach/VideoManageView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f5d5ec817b8626b7610dc7b6ba9d5507e7c2ddf04cb68e73b6ac3ba1625653e
-size 2537
+oid sha256:3264d67a62396813d3bde879ede57fd1b559e363b2d2671d460d0d0191f5e622
+size 3437
