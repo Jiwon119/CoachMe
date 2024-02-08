86dcc03bd9f9f285aa014b6b3e12317c903cdc89
minahpark42 <minahpark42@gmail.com>
2024-02-08 11:18:27 +0900

    완료.추후 수강신청 API 완료되면 테스트 후 업로드 예정
    Former-commit-id: c8261d28e63e1ac6d91f2ad2c8e8c746af1c57b0

--- backend/src/main/java/com/ssafy/api/member/controller/MemberController.java
+++ backend/src/main/java/com/ssafy/api/member/controller/MemberController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dede7d54d05e1b1a6c6ca22253a9def55e1873f1d688bcf1925361c77d396df7
-size 9291
+oid sha256:58d9c989f25501dcee75b390642bc514d57fb53d5834eabe6c50a77f4b279bc8
+size 9828
--- backend/src/main/java/com/ssafy/config/security/token/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/ssafy/config/security/token/JwtAuthenticationFilter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74fb28914d409fbabec0822fce3b693004fcbafba9fd4ece35f4f8e53c7a2db9
-size 5497
+oid sha256:0b1524d6cbb36f3458362e331711f7e080e404c53bca45cccee4ddc8bb25ae9c
+size 5541
