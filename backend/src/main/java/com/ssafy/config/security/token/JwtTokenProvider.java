c328a02bab4d4549243af9ebf6a2bcac77399353
이준학 <0710june@naver.com>
2024-02-08 09:59:14 +0900

    fix: refresh token 검증
    Former-commit-id: cfef2ba4bcfed9c29a039a278424cf1c4bf8b988 [formerly c5643ee760715bf68648b2217d0e687be0ffee9e]
    Former-commit-id: 86751804baac3df57322f92cc78aac20d6d2a723

--- backend/src/main/java/com/ssafy/config/security/token/JwtTokenProvider.java
+++ backend/src/main/java/com/ssafy/config/security/token/JwtTokenProvider.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:afb3587af9b62681e9bd7f57e729c4c8f9215692dafa5f3e6fc107208a0b483c
-size 6778
+oid sha256:d0c4d9c02b2b6c2f50e66a17cc0b8e8c7dc55f7fd609e82c568de6696c44b107
+size 6714
