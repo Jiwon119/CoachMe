757c40c2307dd52759771a7c17b6488ead45ad6c
김지원 <wldnjs35242@naver.com>
2024-02-10 02:11:57 +0900

    fix: 필터 추가, 오타 수정
    Former-commit-id: eaff25ec2e526fdbbc19ab394a0ccd8081d7c4d2

--- backend/src/main/java/com/ssafy/api/coaching/service/CoachingService.java
+++ backend/src/main/java/com/ssafy/api/coaching/service/CoachingService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73cc9aa2a57cc3cb251fcd61a4c28a3da7c899b0dae1281e68fd3e677e524194
-size 8198
+oid sha256:9fa0a1872496955cb83400ab73e8fdb44824df399c6b423fa25de8d956b8f233
+size 8203
