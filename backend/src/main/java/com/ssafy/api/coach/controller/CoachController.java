47233f9df037a6514f64d2040c667d2999c06438
김지원 <wldnjs35242@naver.com>
2024-02-10 03:01:31 +0900

    feat: 인기 코치 조회 api
    Former-commit-id: 753ad5b637b4b536669fddd5df9c96d19cbbb793 [formerly ca7bfdf0444acd788942bdb0c5a6e8bca0aec097]
    Former-commit-id: 08aad2d44dc25e8fc6976a1ad532c70a35d1d379

--- backend/src/main/java/com/ssafy/api/coach/controller/CoachController.java
+++ backend/src/main/java/com/ssafy/api/coach/controller/CoachController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7be34b9b81c1933e4ff2b1dbd38821f213de3f53867d52232db21f1e142a1ca
-size 7723
+oid sha256:bbcd934ad52ef8cee0db905d87c0a2615159431f9557012932a924f7ef316cd6
+size 8112
