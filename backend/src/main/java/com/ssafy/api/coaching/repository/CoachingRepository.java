497896b6bafb69108cb4488f627d1bf445ae3b93
김지원 <wldnjs35242@naver.com>
2024-02-10 00:23:01 +0900

    fix : dm 버그 수정
    Former-commit-id: 549d14996544e2ab8a245d1e7808cb5e697fe179 [formerly 954cd91d6ef4cd3e594cc97326183c7bae950ea8]
    Former-commit-id: 52feeb87cb27cb9abda429de21ff6b03a291181f

--- backend/src/main/java/com/ssafy/api/coaching/repository/CoachingRepository.java
+++ backend/src/main/java/com/ssafy/api/coaching/repository/CoachingRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1872f48a00ab5c6cfce4f8bdb7e59e9bf6c896909d17551cd54eaede395d1fae
-size 2630
+oid sha256:b2311f9a3e1ebcc5c7eeda610ad48c8110a139c106db6344db230b30b24a79f5
+size 2625
