7db2bcd3dc028101c7028375c36f46a886d126c0
이혜지 <lee02g29@naver.com>
2024-02-09 23:03:13 +0900

    feat : 영상 관리 api 연동 추가
    Former-commit-id: 1b60f04e3ed4cebb06c8b55eed21e2d2904cb338 [formerly a5a561a2e9bdbca9a47acdcc161d5ee601517578]
    Former-commit-id: 570a8e9a2ee68be3825a40b52c4834c0f6985f55

--- frontend/coachme/src/components/molecules/CoachDetailCard.vue
+++ frontend/coachme/src/components/molecules/CoachDetailCard.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:339a0a26f6e6e29fce1d1fe8db9f7f23294b7689f2a4e8fa6db8a858f9337fd0
-size 2218
+oid sha256:8c4534680309de1468416b021bfa1a7742524f22d1be34f24498d60a725a34b2
+size 2085
--- frontend/coachme/src/views/search/coach/CoachDetail.vue
+++ frontend/coachme/src/views/search/coach/CoachDetail.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2319108425dd3918fa882d975106e2e3ad8fe6278b6e86b8016e7eb210410c1f
-size 4936
+oid sha256:fd50d6672d965ead54e52eeb15fbc5b8bd9777c37ff91a419f1e6fca575d8453
+size 5235
