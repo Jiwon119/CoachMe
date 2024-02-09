40d34abf5fbe6bba9234ec539d78b92c1f7aa0fc
이준학 <0710june@naver.com>
2024-02-09 23:57:23 +0900

    fix: 코미/코치용 캘린더 데이터 조회 API 수정
    Former-commit-id: d3975da1762a6ed620aa88cdac2cb55af11f0019

--- backend/src/main/java/com/ssafy/api/coach/service/CoachService.java
+++ backend/src/main/java/com/ssafy/api/coach/service/CoachService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7151855824fb37010d7f86f65604081eb22a0960912de5e67543005acd64b00
-size 5868
+oid sha256:ccb55d1c15de14799ecadff21c844c605981bfad1a17ad2a083f8e197ee23c08
+size 4889
--- backend/src/main/java/com/ssafy/api/coaching/mapper/CoachingMapper.java
+++ backend/src/main/java/com/ssafy/api/coaching/mapper/CoachingMapper.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62d78fffa08f689f26db4c11977203cdb25dc567ea5330aaff66384bc3015492
-size 3060
+oid sha256:9a3a2c5b0c066cf8151135809e35d9bc73d2f63fc0450a94fe128397591f168b
+size 3456
