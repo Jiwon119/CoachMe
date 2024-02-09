03f266ee4d3ac752876a3b379d5229d7133496c5
이준학 <0710june@naver.com>
2024-02-09 23:57:23 +0900

    fix: 코미/코치용 캘린더 데이터 조회 API 수정
    Former-commit-id: 40d34abf5fbe6bba9234ec539d78b92c1f7aa0fc [formerly d3975da1762a6ed620aa88cdac2cb55af11f0019]
    Former-commit-id: 3d48f474665e007c585fadd576d25b26bbdb26eb

--- backend/src/main/java/com/ssafy/api/coach/dto/request/CreateLiveRequestDto.java
+++ backend/src/main/java/com/ssafy/api/coach/dto/request/CreateLiveRequestDto.java
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d03c8a48a1b9bb0332070d252471b5fb7949fcfeb85e9ac769ea2af1153392e0
-size 199
+package com.ssafy.api.coach.dto.request;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class CreateLiveRequestDto {
+
+  private Long coachingId;
+  private LocalDateTime date;
+
+}
