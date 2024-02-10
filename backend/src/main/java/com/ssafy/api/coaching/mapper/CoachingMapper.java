a8512510e480561837b8aca3609ee26a726a1755
김지원 <wldnjs35242@naver.com>
2024-02-10 21:46:34 +0900

    feat: 코칭 상세 페이지 조회 coachId 추가
    Former-commit-id: c1eb2984697b1191131bbbb1940ff9a4b0130c21

--- backend/src/main/java/com/ssafy/api/coaching/mapper/CoachingMapper.java
+++ backend/src/main/java/com/ssafy/api/coaching/mapper/CoachingMapper.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ffa94ccb697abfb035b94d768dd49ad26b8771cfb7fc1b489709608afd42b134
-size 3231
+oid sha256:19b0f7ec7e8ca2e4a845f606c8126183768f5b89212a6fd5d22710f3070a9dae
+size 3287
