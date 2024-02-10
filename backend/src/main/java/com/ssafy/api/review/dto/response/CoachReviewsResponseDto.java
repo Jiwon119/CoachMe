8d3f8e1cd32f34afaaffdec521773b938975c177
이준학 <0710june@naver.com>
2024-02-10 18:49:59 +0900

    fix: 리뷰 조회 시 닉네임 추가
    Former-commit-id: 7fb41562ef79f77155bf03cb483a856bd720127f [formerly 709cd21484f12cd919d3d92895e3ff3805dbf14f]
    Former-commit-id: f16689801f8c40ccbc6a3cadd4d0e7ad26099dc4

--- backend/src/main/java/com/ssafy/api/review/dto/response/CoachReviewsResponseDto.java
+++ backend/src/main/java/com/ssafy/api/review/dto/response/CoachReviewsResponseDto.java
@@ -1,11 +1,3 @@
-package com.ssafy.api.review.dto.response;
-
-import lombok.Data;
-
-@Data
-public class CoachReviewsResponseDto {
-  private long coameId;
-  private long coachId;
-  private String comment;
-  private int score;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:819458fa19eaf8f0088c29a2bc04e0f698989f3de61417834806aaa1d531851c
+size 234
--- backend/src/main/java/com/ssafy/api/review/mapper/ReviewMapper.java
+++ backend/src/main/java/com/ssafy/api/review/mapper/ReviewMapper.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66fbd360bb864a662f47bc9845b019a24bf75411bc01481f3c2c83b3f5afee41
-size 1017
+oid sha256:912a83346907c8bb0e407f0a332beba0dbeb6d1383f97c88e1ff99ffc68e0353
+size 1135
