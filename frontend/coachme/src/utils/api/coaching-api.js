80485dded28ef94c49cbfd9b96d12a72adc3b7d0
김동현 <silverstuff@naver.com>
2024-02-11 08:06:09 +0900

    mainpage/api/feat/1
    Former-commit-id: 05224383f4977785d19e13e0267850e7d7190330

--- frontend/coachme/src/utils/api/coaching-api.js
+++ frontend/coachme/src/utils/api/coaching-api.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df2cbd991e0098a72b3638a3fd448045ddc8a7853761b7917157a70abfa0e60d
-size 4862
+oid sha256:47e2381014b1e4a5bc744030bc93256414f527cc6fb3a9539602dede1bc13409
+size 4850
