a06503ff79c7b8e3c869f288963b0aa61da7f49b
SangyeopNam <spearoad15@gmail.com>
2024-02-11 08:12:00 +0900

    fix : api 이름 오류수정
    Former-commit-id: 4c0ce690a4d8af58d02c1f2ead1b09d72dd9c4dd

--- frontend/coachme/src/components/molecules/CoachingCard.vue
+++ frontend/coachme/src/components/molecules/CoachingCard.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e3cb4498cae537fa2069f4006b358e84bbc4ce2743857a75f9d145796a187a41
-size 2689
+oid sha256:0745aa425a58ab3af10ed3fac2e677f108b3321e899796cfb0a79c0c24f2bf83
+size 2625
