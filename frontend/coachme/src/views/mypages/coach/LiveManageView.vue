b41be6b195600fe369014fdef5ecc048283833b9
SangyeopNam <spearoad15@gmail.com>
2024-02-10 18:25:49 +0900

    fix : date key 오류 해결
    Former-commit-id: feafffc24c3bda915b64bedfe8c1faf886cba2a8 [formerly b416eebf792a1fee23361dbf777b56992cf13735]
    Former-commit-id: 5a638ab2a2ac90d846aa8dc6189886bff0c5660d

--- frontend/coachme/src/views/mypages/coach/LiveManageView.vue
+++ frontend/coachme/src/views/mypages/coach/LiveManageView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3da64d3ca2b3b3cfbcc5fb3ca113c185702180914cf2e4dc93e37ab4db4b936a
-size 8432
+oid sha256:be708da82002d435f4a8eaffc7535915fbc5468af4477c7262e5a23412843b0f
+size 8395
