b010946819a649c165c942346788b2f4ff51fa1e
SangyeopNam <spearoad15@gmail.com>
2024-02-10 21:22:34 +0900

    feat : 코칭 찾기페이지에서 코치이름 추가
    Former-commit-id: 5728d8ad0e6f6b0829a808a215fb3fa9a8a186cb

--- frontend/coachme/src/components/molecules/SearchCoachingList.vue
+++ frontend/coachme/src/components/molecules/SearchCoachingList.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7876a0f06e3369ddb5f569b9dc30895119cae3d7d35bb5d5d96e6023e11cfca2
-size 4587
+oid sha256:ffde67509327335b4d046519d5c012fa12c03d34bbe9d8285aeca182c502fd24
+size 4599
