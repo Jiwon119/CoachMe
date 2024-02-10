7d50aa4a427bc45227d1f300ba852fa51532fdaf
남상엽 <spearoad15@gmail.com>
2024-02-10 21:16:54 +0900

    coaching-리스트-조회/feat/2
    Former-commit-id: 23eeec4c96ecc84e11d932b6e2f3cc70115430a2 [formerly 32943eca806e5090996aa48a75bd5ca717c7fcb9]
    Former-commit-id: 277fbfd6c28ba7bce037ac338266d04271633816

--- frontend/coachme/src/stores/chat-status.js
+++ frontend/coachme/src/stores/chat-status.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72d0867d7e03f7ab4424d6318de50678908ab80f19ca0fb0f6aba0d117804b99
-size 2199
+oid sha256:91831d027dff706538a3b8651f66d54da320cda722aac25d337313c60c26bcd2
+size 2228
---
+++ frontend/coachme/src/stores/coaching.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1a0d444c1d770e0e9bccde73f0767ac2057c4d356aa7410c632c7223c2dd8bb8
+size 155
