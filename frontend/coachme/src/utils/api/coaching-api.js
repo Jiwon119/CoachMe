8f08c9590607a259580703bbe6bf45136b0e1fae
SangyeopNam <spearoad15@gmail.com>
2024-02-11 08:12:47 +0900

    fix : api 이름 오류수정
    Former-commit-id: 06af59bd8430e42bbda3bdcd917d9be210d93b15

--- frontend/coachme/src/utils/api/coaching-api.js
+++ frontend/coachme/src/utils/api/coaching-api.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47e2381014b1e4a5bc744030bc93256414f527cc6fb3a9539602dede1bc13409
+oid sha256:92e822aacde8a435b32af9de05f37e448a8d92dc74a14991451f433a786a69f5
 size 4850
