b24ed2c0e092087cbd01b9c2a5673c50638da2eb
이준학 <0710june@naver.com>
2024-02-10 20:41:37 +0900

    fix: 코칭 상세페이지 카테고리 추가
    Former-commit-id: ce3968ff70841a3568116827a8588ed4de6aabdd [formerly e0e468cf9bc2f7492815be5ff163481f67f9e90f]
    Former-commit-id: bb21765c983593ea1d086336d51d1a06e8f60b58

--- backend/src/main/java/com/ssafy/api/coaching/dto/response/CoachingDetailResponseDto.java
+++ backend/src/main/java/com/ssafy/api/coaching/dto/response/CoachingDetailResponseDto.java
@@ -1,17 +1,3 @@
-package com.ssafy.api.coaching.dto.response;
-
-import lombok.Data;
-
-@Data
-public class CoachingDetailResponseDto {
-  private String coachName;
-  private String coachProfileImageUrl;
-  private String coachingName;
-  private String htmlDocs;
-  private String summary;
-
-  private long reviewCount;
-  private float reviewAvg;
-  private long likeCount;
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:5057e2d490f998efb34d7423a20650d3be84c7a555436f4cfc08b4552bc5f296
+size 448
