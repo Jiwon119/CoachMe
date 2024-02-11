9ef7b04c677c6c7459a27dbc172fb173291db807
minahpark42 <minahpark42@gmail.com>
2024-02-11 17:23:56 +0900

    찜콩 like 1-6까지, member-16 API 완료
    Former-commit-id: d26325cd1bb6017eda493e44a0b59ada199c569a [formerly d96d4f8a2918f3f97680b49af73d31c59fe8d087]
    Former-commit-id: d03357e8015f3ddf6dbfa754c3a9292fcdff9fb2

--- backend/src/main/java/com/ssafy/api/coaching/repository/LiveCoachingRepository.java
+++ backend/src/main/java/com/ssafy/api/coaching/repository/LiveCoachingRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22bf2e4e11ba505e2ebe2fd20d581c0dba469209a607e1f18be8ee22c9719de2
-size 919
+oid sha256:46ca4791875162a5e60d527dd95d23a7fa0c2c0b18ddb6d2016fd5e689ce77de
+size 608
