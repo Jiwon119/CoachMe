33df209b7ef7dcd05fbc7f6aaea9790be10bb6c9
남상엽 <spearoad15@gmail.com>
2024-02-11 22:54:09 +0900

    live-coaching-openvidu
    Former-commit-id: d2dc771d18cab6e3519f96cb1342e92a1a4d87a6

--- frontend/coachme/src/components/molecules/LoginNavBar.vue
+++ frontend/coachme/src/components/molecules/LoginNavBar.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:980c22632d9f668b10364f9ec111e446c3eb240f2d3563f05dc3c6da06845f74
-size 2437
+oid sha256:32b7370eab149584fc80b1620e220bc1fe8fd8de85755c76f28743788220fcf4
+size 2388
--- frontend/coachme/src/router/indexes/main-index.js
+++ frontend/coachme/src/router/indexes/main-index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9666991fe0b8e354d65abac022e0cca76de49592bf591d87d698468d777488c1
-size 636
+oid sha256:0133a18743f948003e45e2b1a34e62e25009901448e1bec9f49d4cf896d1fdbb
+size 750
