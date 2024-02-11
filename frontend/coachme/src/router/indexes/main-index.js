3cd3e6d0572461fb0bc8b18d91762066a9f273c3
SangyeopNam <spearoad15@gmail.com>
2024-02-11 23:03:28 +0900

    fix : 화상페이지 오류 수정
    Former-commit-id: c1db5328ac705c510a388fa7a2ff8540237ef018 [formerly 54324a485fd06a222dd1a9c6e5da1a2a10f16929]
    Former-commit-id: d7acfd6a7045c8b46955c8af570e277af2b2979c

--- frontend/coachme/src/router/indexes/main-index.js
+++ frontend/coachme/src/router/indexes/main-index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:56c51e2f618d475cdfc65e1450f03d40a181ffd79b1d2bc732e3907f9b502c36
+oid sha256:1198900246cdb44007b9a8ecfd23fde2c4361a6d786fb0e718a32336ac97c8c3
 size 750
