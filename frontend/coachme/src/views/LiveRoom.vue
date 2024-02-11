16fe7e2408d9484f96bfa73b4e1f5dc04746fe50
SangyeopNam <spearoad15@gmail.com>
2024-02-11 23:00:51 +0900

    fix : 화상페이지 오류 수정
    Former-commit-id: bf925cebd97ad1d8978d5c440b11512e40f283ab [formerly bad7eab349b8911f30e01dc68617735f727f206c]
    Former-commit-id: 848bef8d4bfe766f7b0f90e581aa03bf8fc785f3

--- frontend/coachme/src/views/LiveRoom.vue
+++ frontend/coachme/src/views/LiveRoom.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c229a43acab520226590e83734736ebd962f0bcde378fb258e182ffddee9d13c
-size 10326
+oid sha256:294a582b08efb2d962b93016ed8c4c5ed63b567405f68fe49fa059a9dda28da8
+size 10350
