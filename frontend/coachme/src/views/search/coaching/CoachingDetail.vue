02cfabcd481dd2143069776169ffc264109fa4f3
SangyeopNam <spearoad15@gmail.com>
2024-02-11 13:56:49 +0900

    refact : 코칭 상세페이지 레이아웃 일부 수정
    Former-commit-id: 991995cc6fb1c6d4dd61be6de13d1fa9ff52a38f

--- frontend/coachme/src/views/search/coaching/CoachingDetail.vue
+++ frontend/coachme/src/views/search/coaching/CoachingDetail.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e98d42de4c4df666e4162383ee43929f70c0f6a8e2b56bdf650f212d7b2e657e
-size 9926
+oid sha256:0763ee0553c12f9fb384104762aa4f32b5693212bbbf2d64324d092b2e74a4a0
+size 11037
