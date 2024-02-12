bf694d5db6189dd7dc5b281de7aa74f9d8892e09
김지원 <wldnjs35242@naver.com>
2024-02-12 20:50:29 +0900

    token, memberId, memberName, memberProfileUrl 사용 예제 추가
    Former-commit-id: a952a22cabe8dbb88ad8bbb3349cfa1e479a2da2 [formerly 91625e7acc66da0673bb6efdc3323c5d1266a67b]
    Former-commit-id: fa51e29a089827a832f727dac4366038683354e4

--- openvidu-vue/src/App.vue
+++ openvidu-vue/src/App.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34b61de4114a9828fb6d4dae049c1fb250d274f8c27b70462efa6d6fc95c25a1
-size 10130
+oid sha256:0628b27c36258a761a1c2a2207dde0fc034c6b051cd888a63da9acbb994d7ffb
+size 10369
