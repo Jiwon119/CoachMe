92ccbba7edf2d2c3202eb9019791c16b2c89c08e
KDH <silverstuff@naver.com>
2024-02-12 17:44:13 +0900

    fix : 코치, 코칭 카드 태그 수정
    Former-commit-id: 588d103d82658ef1bcbfdceea0009972975ab63d [formerly 2b68eebae42d3e7a791a78ab69f78864d8ce964f]
    Former-commit-id: 2f51cb9dfe98e70dc7e7d2264ce14f00560844a5

--- frontend/coachme/src/views/MainView.vue
+++ frontend/coachme/src/views/MainView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c5a1444c400d4a103496c3b2aac8d32fb0a4f60ab63f96ce37b634be46640b1
-size 10658
+oid sha256:05687adc258953fb928048dbc321849e1efaf507e0dbdb8c317a7d3cb5b878fe
+size 10668
