8b495c998da2edcd3e0e40cb4daf9aa6b0cde9b9
SangyeopNam <spearoad15@gmail.com>
2024-02-12 17:04:16 +0900

    video test
    Former-commit-id: 1f77617b5c92dccbe43247099a33ba7ec8a24adf

--- frontend/coachme/src/components/molecules/CoachingCard.vue
+++ frontend/coachme/src/components/molecules/CoachingCard.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1daee5d98b355fc83dcb1419a1477c0b858dea0e0efd64cae5d16eea53763ee
-size 2738
+oid sha256:024c07ed2bb6d3cde2ab255c9d8f4cae87c1f9750ab31f97f8b8614137172bba
+size 2729
--- frontend/coachme/src/views/MainView.vue
+++ frontend/coachme/src/views/MainView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cccd50d3e16dd8240e99690ce372109f271005f5fb7adc7253b43dbc9de23181
-size 10715
+oid sha256:cdc832f0a8ae9c7567047e86c1c7e354ae630332128a9f9a28114ebbc7446780
+size 10667
