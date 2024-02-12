cd96b79520f97ca8f9f3d945b4fda23519503e68
김지원 <wldnjs35242@naver.com>
2024-02-13 08:40:06 +0900

    화면 녹화 기능 구현
    Former-commit-id: 67390ba53bff3893b906ef5080df173bc7394fe6 [formerly 43bc48ba6c96e9c65a9937fb8685ff06ffc22760]
    Former-commit-id: 580ea1895f408141ca938f8caccc4fb3ce6b527e

---
+++ openvidu-vue/src/AppTest.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d581437aaf7bd2fab23f09f41e2fdb9ee44a732b48c6cd4cd3c1a3c2d86d38c5
+size 2636
---
+++ openvidu-vue/src/components/RecordingView.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c2c719d6e1abd053c0c81230314181675c72c1ff5dd8747f951d5b309a10cd71
+size 4558
