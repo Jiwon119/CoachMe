a5cb8d5eedf8ff0cc7105f63d60b48092bf35dc6
김동현 <silverstuff@naver.com>
2024-02-12 11:02:37 +0900

    Mainpage/api/feat/1 fix : like-api
    Former-commit-id: fc6adb3c8d1ef7c94498a9c4fa9416e9d1e0844c [formerly e8481593451b36246e443defe1f434aef42e2910]
    Former-commit-id: a6806578ca9cb0368ff7d2ba5018ec3cfc0cbc74

--- frontend/coachme/src/stores/coach.js
+++ frontend/coachme/src/stores/coach.js
@@ -1,9 +1,3 @@
-import { defineStore } from 'pinia'
-
-export const useCoachStore = defineStore('coach', {
-  state: () => {
-    return {
-      coaches: []
-    }
-  }
-})
+version https://git-lfs.github.com/spec/v1
+oid sha256:7e03ace97743f3a8db3bc009e79d5daddb3b5b2ae99c347c2112c0319cb4013b
+size 150
--- frontend/coachme/src/stores/coaching.js
+++ frontend/coachme/src/stores/coaching.js
@@ -1,9 +1,3 @@
-import { defineStore } from 'pinia'
-
-export const useCoachingStore = defineStore('coach', {
-  state: () => {
-    return {
-      coachings: []
-    }
-  }
-})
+version https://git-lfs.github.com/spec/v1
+oid sha256:1a0d444c1d770e0e9bccde73f0767ac2057c4d356aa7410c632c7223c2dd8bb8
+size 155
