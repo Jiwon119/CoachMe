4793403461cb012c6c3e391c871f884739cc9968
이혜지 <lee02g29@naver.com>
2024-02-12 11:03:47 +0900

    찜콩조회-API연동/feat/3
    Former-commit-id: 4c8ce2292511fc553d931978781241267e56a2e8 [formerly 992e4e2b373ecad6b4e03d5b5d928863cb00db73]
    Former-commit-id: cc40141c9ebf907e5b5771a0f64f9f8d92296b58

--- frontend/coachme/src/components/molecules/CoachingCard.vue
+++ frontend/coachme/src/components/molecules/CoachingCard.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7dc5df90ae5042d6d9fbee9708c40ab7a8b68f8a840907584419cfce75f69f47
-size 2625
+oid sha256:b7427f02f66d54c4307a2b9565fed9ee7bcc1fa3f94d3c27efb8d64ef93b8fdf
+size 2731
