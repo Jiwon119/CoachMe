70e1edab1edc920577f38b05e6dded94e8c778ff
이혜지 <lee02g29@naver.com>
2024-02-13 17:57:27 +0900

    fix : 대표영상 미리보기 수정
    Former-commit-id: c71cf3aa6c654b493de2327d47c26fa133096c8e [formerly 47044a5a4df81d0865bedd3f41297e02d8093378]
    Former-commit-id: 722c0d0ac9d53c764c6aee276ade6a397b09125c

--- frontend/coachme/src/views/mypages/coach/VideoManageView.vue
+++ frontend/coachme/src/views/mypages/coach/VideoManageView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e05f95f144ff07b881db9cf4ef42d96af83fc84886fac826412968ccaa44ecda
-size 4417
+oid sha256:5ab42cfe413b16a71ccef115cdd2d3913742c93e7d0a472ea484265c6757d67f
+size 3318
