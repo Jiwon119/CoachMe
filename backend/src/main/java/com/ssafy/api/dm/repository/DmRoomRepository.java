a85597cdbfbe65b9e42462db390f64631818bb60
김지원 <wldnjs35242@naver.com>
2024-02-13 14:59:38 +0900

    fix: Dm room 조회 에러 수정
    Former-commit-id: 2b42a3a1b10aabd6e740575eb37aaac68c6fc0d6

--- backend/src/main/java/com/ssafy/api/dm/repository/DmRoomRepository.java
+++ backend/src/main/java/com/ssafy/api/dm/repository/DmRoomRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b83d7293419542d2525ad2e8e5825fa184e564428f577fead3d048760d1012b1
-size 706
+oid sha256:f8d765ed95423c8f308c111a8224e763c5ed9a98ab15b8356eb6c89b01d2bd44
+size 721
