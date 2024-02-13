ccd9dc6919e5a14e8b748bed637b8ecadd4f4306
KDH <silverstuff@naver.com>
2024-02-14 01:44:27 +0900

    add : btn
    Former-commit-id: 19eccff48279a213aa2d708f83cb13b964a7e08f [formerly ccee02fa2c2239186ce373ea6f045e142d850394]
    Former-commit-id: ac41954d87b9cbfd44577d6319bf0b5c09011053

--- frontend/coachme/src/components/openvidu/OvVideo.vue
+++ frontend/coachme/src/components/openvidu/OvVideo.vue
@@ -1,3 +1,17 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f48602efa8ce5b9581f36be6bf28f0fba49431a9d0105eb4aade34d5a18824c7
-size 213
+<template>
+  <video autoplay />
+</template>
+
+<script>
+export default {
+  name: 'OvVideo',
+
+  props: {
+    streamManager: Object
+  },
+
+  mounted() {
+    this.streamManager.addVideoElement(this.$el)
+  }
+}
+</script>
