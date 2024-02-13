a015b1d41f2abb6a64c3b9e8281501dbe1b88df1
SangyeopNam <spearoad15@gmail.com>
2024-02-14 06:08:24 +0900

    fix : 채팅 시 로그인 체크
    Former-commit-id: e7ae7489eadaccd996ecbb5a5f0e705beb4790fd

--- frontend/coachme/src/stores/chat-status.js
+++ frontend/coachme/src/stores/chat-status.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b75d3d409d9647fb7e86047c65c163738538dee0272be2d673fefd475d0bb9d0
-size 2453
+oid sha256:4d6c506ac3170e994a0b55cf57a9d92718ce33930fe58e25f2aed5aec782418d
+size 2768
