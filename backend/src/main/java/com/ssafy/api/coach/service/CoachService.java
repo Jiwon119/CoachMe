b1bb8b5002d0dc4593d80aec51f5206ca7533032
김지원 <wldnjs35242@naver.com>
2024-02-13 23:26:10 +0900

    fix: 코치 검색 오류 수정
    Former-commit-id: 81fa9b551682b5796f168065107876b345d8c3a8 [formerly 36adb6891c2030fa484ed4f6367d6976275bd734]
    Former-commit-id: d3fd99a0da1ed94b5fd79a319efaa58e0d90f96d

--- backend/src/main/java/com/ssafy/api/coach/service/CoachService.java
+++ backend/src/main/java/com/ssafy/api/coach/service/CoachService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf4e981681783790a24b93b1617695ba33cccc57d2c02aa227fe96c23d1bf9a9
-size 6455
+oid sha256:e1e83ab7c871b44050f31e6e95c3298b69611710b2a42c27cb63cd3e90cd7446
+size 6452
--- backend/src/main/java/com/ssafy/api/coaching/repository/CoachingRepository.java
+++ backend/src/main/java/com/ssafy/api/coaching/repository/CoachingRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4640a2cef077fbaa267d1adc0ecda398fd4f184fd273d6da144cda71ad8bd732
-size 1099
+oid sha256:7eebb13b846d9d533453b8e35ad4a6b3360f6a080df4c05754dddff1b1581d07
+size 1559
