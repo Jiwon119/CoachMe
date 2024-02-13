54a745070aaf6db3787281a9c171cbe137dba3b7
김동현 <silverstuff@naver.com>
2024-02-13 21:13:37 +0900

    fix : swal-font
    Former-commit-id: c7f2f3bbc3ce5e0b2e61e6b45136d9ed3ad11fbf [formerly 42bb7bf4fd94caf5548b3d41ceaeb97bdc0796df]
    Former-commit-id: 98758fa8959ba464c16f697fcd7ad59542ad6f6e

--- frontend/coachme/src/components/openvidu/OvVideo.vue
+++ frontend/coachme/src/components/openvidu/OvVideo.vue
@@ -1,17 +1,3 @@
-<template>
-	<video autoplay/>
-</template>
-
-<script>
-export default {
-	name: 'OvVideo',
-
-	props: {
-		streamManager: Object,
-	},
-
-	mounted () {
-		this.streamManager.addVideoElement(this.$el);
-	},
-};
-</script>
+version https://git-lfs.github.com/spec/v1
+oid sha256:10ed47d28927772cfa6bcc3baa10e86ae4e474361a0613b327d6c6f3ca816e87
+size 207
