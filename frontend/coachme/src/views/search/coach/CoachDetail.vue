a5d239cd13fa34590ca514aca66a82aecdf72f46
Sangyeop Nam <spearoad15@gmail.com>
2024-02-13 18:07:16 +0900

    fix : 리뷰 일부 수정
    Former-commit-id: 10abd1bebe0df4517979597bb1e97ddf76481e46

--- frontend/coachme/src/views/search/coach/CoachDetail.vue
+++ frontend/coachme/src/views/search/coach/CoachDetail.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24812deb6667f3f298374a7e0d2ffa455d995876df2d9f4c861283a9af7e4571
-size 11695
+oid sha256:b7e404ee374188c68b697c655171bf8e3a43a27ec8e90f9bd7d6f4977dbd1dd7
+size 11377
