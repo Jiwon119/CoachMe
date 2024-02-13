f072297b5cae400baa4dce87c9ab683442c460cd
이혜지 <lee02g29@naver.com>
2024-02-13 14:59:34 +0900

    찜콩조회-API연동/feat/3
    Former-commit-id: 25d39cd6cdccc441ab19d065cdf3ac1c04450168 [formerly f536e414c94eb5bc67c174515fad8847b7c8139c]
    Former-commit-id: cc250bb424e0cc7d07437e761df624b841e53a33

--- frontend/coachme/src/components/molecules/CoachChatBox.vue
+++ frontend/coachme/src/components/molecules/CoachChatBox.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cab7c1b0108a6a6e970da3b129c4f95e485ddeb7ac36f3674229da5e044dccbf
-size 1441
+oid sha256:b903fe34ec4a89e12e72c2f5809074163a64830ff39b757657ad7e82c8f68712
+size 2596
--- frontend/coachme/src/components/molecules/ReviewDetailCard.vue
+++ frontend/coachme/src/components/molecules/ReviewDetailCard.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b237b6a554ad0f5c378b6f89542b1dc2b2dab82b2acb72574a11c3efd5439eab
-size 3257
+oid sha256:7383d3dbccac3e9b6d670a5a4e92c1273f893a4e21b610753aeee84df7135b48
+size 3288
