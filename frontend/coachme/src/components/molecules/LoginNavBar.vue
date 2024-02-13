086406f4daa712b2445885d51b58b2a4f1831fa4
남상엽 <spearoad15@gmail.com>
2024-02-13 20:46:23 +0900

    layout-fix/2
    Former-commit-id: fcda600dae4b6167ed181e752ae1d0e1536c8f2b

--- frontend/coachme/src/components/molecules/LoginNavBar.vue
+++ frontend/coachme/src/components/molecules/LoginNavBar.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e0b62d697cc7d70f90e2e95580da28bde360dc0dae41a0e9afd11c55881ba559
-size 2465
+oid sha256:8fcb85a87e17d19c639c0a03f0de78fc336b16f5b22a8fd950287d7f3b4be60b
+size 2779
