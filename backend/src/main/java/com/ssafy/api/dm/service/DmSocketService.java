13022a415b69aba8d1d5144b7a8c510aa556aeb5
김지원 <wldnjs35242@naver.com>
2024-02-13 20:49:49 +0900

    Update DmSocketService.java
    Former-commit-id: db4d5644354cb47d61c01e4b2f5119f95ddc3aa1

--- backend/src/main/java/com/ssafy/api/dm/service/DmSocketService.java
+++ backend/src/main/java/com/ssafy/api/dm/service/DmSocketService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ca686c342738ee43d2d24cb505fff2b1f8f259c25b27e1927f87de0063f4702
-size 1677
+oid sha256:4cedeeb450e0f7234a55721591d65cada30dd64a1534fb98dd156c8ccb0db899
+size 1907
