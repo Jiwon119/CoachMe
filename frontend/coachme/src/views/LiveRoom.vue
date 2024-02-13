a8afb62d672167cfbd5a62a08a51b4220df6c913
SangyeopNam <spearoad15@gmail.com>
2024-02-13 21:53:08 +0900

    feat : dm 수정
    Former-commit-id: 11e9f16d74d246bc15c6787aca63bf8474d9bfc4

--- frontend/coachme/src/views/LiveRoom.vue
+++ frontend/coachme/src/views/LiveRoom.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:340d4dc7c4eca0bf4df84664ee746157802af75308212a40ce60fa68d2f2d756
-size 11558
+oid sha256:fd76960f5c649f648bf1bba31c353c35a4dd20ead36510115a003396cffd6eb2
+size 11590
