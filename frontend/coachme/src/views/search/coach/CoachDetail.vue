045bf83e2ac2f26449b2c71ac056457b10b09362
Sangyeop Nam <spearoad15@gmail.com>
2024-02-13 17:45:16 +0900

    feat : 코칭 상세페이지 일부수정
    Former-commit-id: a731fb39903634c9862efee519b407bfe5ba2f6d [formerly cc7311da3c71fd960409537fb8d601ba5154453f]
    Former-commit-id: 7774f51ccb31c29b7521a6a51b81020cbdbbc181

--- frontend/coachme/src/views/search/coach/CoachDetail.vue
+++ frontend/coachme/src/views/search/coach/CoachDetail.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74731d1b6ee732fc7e8be66e89c1120660789f5486378fae351c347ba6fbb68b
-size 6122
+oid sha256:721748eb41c3b0eb8dd6e653196956542402a3e017638887c954a6bf2ce81aec
+size 10451
--- frontend/coachme/src/views/search/coaching/CoachingDetail.vue
+++ frontend/coachme/src/views/search/coaching/CoachingDetail.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b8c18bc1060cf14b085c543edb3815f2b3b520d9a66e26643df7d3d164f7f12
-size 12233
+oid sha256:46d8868c3e7bd4156368f0ce71e8bde4f48cbb5c7f806e43d158290f1e02eb12
+size 12510
