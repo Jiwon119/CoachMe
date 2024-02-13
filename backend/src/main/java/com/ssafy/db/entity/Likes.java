1d281090af63fa00f55e241d6c904b74a1e4e9df
minahpark42 <minahpark42@gmail.com>
2024-02-13 18:04:11 +0900

    발표 준비 민아
    Former-commit-id: cd218c83f77263b481a224db649d6789a26ebc00 [formerly b39f4413f8d14036086bc7951603e3fea96bba0f]
    Former-commit-id: 333b3b254261a0493bd3b16584d31295efbca8d8

--- backend/src/main/java/com/ssafy/db/entity/Likes.java
+++ backend/src/main/java/com/ssafy/db/entity/Likes.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:38b0b72494688046d94f05f82020a555a47c4716bfc2fa11338e3e17616fbc95
-size 1020
+oid sha256:5ebd6052d7f03e0e42a851b8f904a514d114c5197f7798f805bce0bb64c01aa3
+size 1188
