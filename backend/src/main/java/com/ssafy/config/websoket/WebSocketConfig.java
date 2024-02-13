fe1215dadc06e916fb210b626416359bc90c0d6f
김지원 <wldnjs35242@naver.com>
2024-02-13 21:57:52 +0900

    Update WebSocketConfig.java
    Former-commit-id: 7b611eaec3510f4647eb95e9140776016efeb5cb

--- backend/src/main/java/com/ssafy/config/websoket/WebSocketConfig.java
+++ backend/src/main/java/com/ssafy/config/websoket/WebSocketConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb366e7caac06aaa56c3d69f0c50bae949b2c1beaef6e1cd8f8d676e11d571a2
-size 1311
+oid sha256:7b4c9837df9b857e45dde19af748908baa68bb587d60fd88b003d8d6879c7b31
+size 1303
