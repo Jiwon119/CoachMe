cc85cfb5cfc04075a5e1e1e3624094c79933e5a5
KDH <silverstuff@naver.com>
2024-02-13 14:48:11 +0900

    feat : 1,2,3 등 메달 추가
    Former-commit-id: 98f0db256486935eb404107e1b96f8aa2068a923

---
+++ frontend/coachme/src/components/molecules/MainpageCoachCard.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fd0bc9f9e6f60ba98f918ef822dfb6f546e5a2baa420435d6fc4186957fa62f9
+size 2937
--- frontend/coachme/src/views/MainView.vue
+++ frontend/coachme/src/views/MainView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15044f2b89a39d343dd675d104052cb9391ce3fcb02bfed1482fa9c9c6f509db
-size 11511
+oid sha256:e3499c2786be2be9bd27b12b612f26c56555782bd154275cd0c0077068c9a376
+size 11625
