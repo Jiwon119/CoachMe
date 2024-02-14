88e64c37feab9634cd97c158c04939547b47e731
Sangyeop Nam <spearoad15@gmail.com>
2024-02-14 16:43:51 +0900

    fix : 권한상승 요청 수정
    Former-commit-id: c6602b7787dbde2908d5a984a7c8abccdc0a68f4

--- frontend/coachme/src/components/molecules/MypageSidebar.vue
+++ frontend/coachme/src/components/molecules/MypageSidebar.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6048271979ccbe2d81f3a1b73ab8d5a6cbbb24ea9b6ac7fe21838a5ccf01b665
+oid sha256:cce1ccef75a74ce28c5f2c0c0c8182a3376a0f696e27dbcec2a14d2eb4d0c6c8
 size 4386
