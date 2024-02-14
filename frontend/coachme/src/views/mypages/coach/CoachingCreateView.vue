6c8dac0901afd64b1eec65691fe2a7fe84b35b96
Sangyeop Nam <spearoad15@gmail.com>
2024-02-14 17:12:36 +0900

    fix : 코칭 생성 수정
    Former-commit-id: 39f16517403910c6b7a79356db915624a5e92ed2

--- frontend/coachme/src/views/mypages/coach/CoachingCreateView.vue
+++ frontend/coachme/src/views/mypages/coach/CoachingCreateView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2cbda64ab727ded269a88613db756bbaf5f8959d6c8c86ea34fc3c3c9fec93b
-size 6695
+oid sha256:29a3e6e03219d7739d40a2d973f8160b6fe4b15dc10cfa32bb4e61606614f591
+size 6728
