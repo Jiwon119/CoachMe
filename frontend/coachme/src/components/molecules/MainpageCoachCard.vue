fb7eb604c06f40dc19ee2c9cdd7060553e7a0aeb
Sangyeop Nam <spearoad15@gmail.com>
2024-02-14 09:46:08 +0900

    feat : header 토큰 추가
    Former-commit-id: 1131b6db9d43d8e454370a57b58bda13587f65af [formerly 3282502ab0802a1a928080dc097f3834e7f585be]
    Former-commit-id: 2b4990c93fa590f70c224cf84279408fe8fb6c1b

--- frontend/coachme/src/components/molecules/MainpageCoachCard.vue
+++ frontend/coachme/src/components/molecules/MainpageCoachCard.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:71713b5e0182cb0766a66952b7e9553252863ac88d1df35bdf0b412e9e357d0f
-size 3058
+oid sha256:9239c13d1e406637f7da4f08c1337ed64b1cb4173e36be951fce62b92419cbfa
+size 3045
