c8c3ef319b70249bf7672719e753bc47d5df3e98
김지원 <wldnjs35242@naver.com>
2024-02-14 12:23:32 +0900

    fix : openVidu member profileText 추가
    Former-commit-id: 8676824a1cdb570557a78577bb77c445f2477fbc [formerly c9f38c76494dd5dea5aead86d400cebd68332f8a]
    Former-commit-id: 9acf69a80013bf3bb988609c3f1f3e95b0f414a7

--- backend/src/main/java/com/ssafy/api/livecoaching/dto/response/LiveCoachingMemberResponseDto.java
+++ backend/src/main/java/com/ssafy/api/livecoaching/dto/response/LiveCoachingMemberResponseDto.java
@@ -1,11 +1,3 @@
-package com.ssafy.api.livecoaching.dto.response;
-
-import lombok.Data;
-
-@Data
-public class LiveCoachingMemberResponseDto {
-  private String token;
-  private long memberId;
-  private String memberProfileUrl;
-  private String memberName;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:65b737b634059d376876287b47da5a95d58db2bd8506631552cfa8907c645144
+size 274
