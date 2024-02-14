7622ee46a1b0e745f79cfc995f7c1e0dd5a66ffc
김지원 <wldnjs35242@naver.com>
2024-02-14 14:35:54 +0900

    feat: 녹화 api 구현
    Former-commit-id: 941168c92f7bd0c1806cc1a2cebab5e0cd75d016

--- backend/src/main/java/com/ssafy/api/livecoaching/controller/LiveCoachingController.java
+++ backend/src/main/java/com/ssafy/api/livecoaching/controller/LiveCoachingController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:36481572b4fc712ee2c3c3cec1add5bc995a10e56a8e1fc971f567dc101a55e6
-size 6626
+oid sha256:b861cb35651aa94ededad264ba46dc35e865b5dfd64818c11c3ad50641583831
+size 5505
