15beba1dfb2b2f7203a7bdc5430b21081a39b79d
김지원 <wldnjs35242@naver.com>
2024-02-14 19:56:55 +0900

    fix: 캘린더 코칭 정보 api 코치아이디 추가
    Former-commit-id: 8fcfff15554de58f461d5402f8a29747669a8a82 [formerly f332849c527715cd23854ea3f2deab9699055928]
    Former-commit-id: ca92a229c82bbd5d5894f64f2d2c9fe0acd14436

--- backend/src/main/java/com/ssafy/api/coach/dto/response/CalendarResponseDto.java
+++ backend/src/main/java/com/ssafy/api/coach/dto/response/CalendarResponseDto.java
@@ -1,15 +1,3 @@
-package com.ssafy.api.coach.dto.response;
-
-import lombok.Data;
-
-import java.time.LocalDateTime;
-
-@Data
-public class CalendarResponseDto {
-
-  private Long id;
-  private String className;
-  private LocalDateTime date;
-  private Boolean status;
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:fba87b179a780c5f0f2e313b057d5c05e643e49859dec1828a52f175b7ae502d
+size 268
