d3f4e4c2d3934ea893c04ff8c9aa251137590acb
Sangyeop Nam <spearoad15@gmail.com>
2024-02-14 15:10:56 +0900

    feat : 녹화기능 추가
    Former-commit-id: 6277c8bc092f70d2bf12470a1816f3b999086663 [formerly 0e1d756d6b8f45a495a10002c468eb5854dcf1dd]
    Former-commit-id: ccc70d1d5a4f1072cef7af6149795f4f6fe6ae49

--- frontend/coachme/src/utils/api/livecoaching-api.js
+++ frontend/coachme/src/utils/api/livecoaching-api.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19df993f43829f0cea2eb84972187ebb1a06ba1a33df17125f57057e869b9402
-size 1557
+oid sha256:7ba2e3df23490c7a63cc36027802fae284140f0df39ab814c3c2c6118ec67998
+size 3185
--- frontend/coachme/src/views/LiveRoom.vue
+++ frontend/coachme/src/views/LiveRoom.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:830049d6e4cd709d1a757538d8839007d9962746097425eeb9f6bd1ef7d46cef
-size 12405
+oid sha256:e1298d91c297bcdc161c953bcf7ce92b9f7cd2b78857e736cc74c27b763534da
+size 13437
