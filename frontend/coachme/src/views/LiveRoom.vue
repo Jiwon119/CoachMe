246c4cfd70005cd2386678498f32fce2e3be1446
KDH <silverstuff@naver.com>
2024-02-14 13:23:04 +0900

    fix : livescroll
    Former-commit-id: 54d5cbef4fb2fd1c6d09ca91ec1e0fcd9a41329d

--- frontend/coachme/src/views/LiveRoom.vue
+++ frontend/coachme/src/views/LiveRoom.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c689a4e07d9c9430d2a5945d5365910b6494f0c74cb071ffab2bf634ad927e83
-size 12113
+oid sha256:717be7df99566c70f8664d7b83bba5efffaee7d93fb6b11edff3a2e345ce1b37
+size 12472
