6dfcad40fd8aa16e61368c841d66d63ade1579fa
김지원 <wldnjs35242@naver.com>
2024-02-14 09:55:02 +0900

    fix: dm 에러 수정
    Former-commit-id: dc0159f80dab95a0d779905d1b1fddb80e675e8b

--- backend/src/main/java/com/ssafy/config/websoket/ChatPreHandler.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:979b08153c8269aae9e3cf3e0f3ee660992d8e98458801e2b8d67f8766be01f5
-size 2520
--- backend/src/main/java/com/ssafy/config/websoket/StompExceptionHandler.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:133c4443861d7237e65be652bd04709cda23f9bdcb28cb15d27b8b955d843013
-size 3806
