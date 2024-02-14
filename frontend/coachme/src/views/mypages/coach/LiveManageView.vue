2acc2025699a058ea8aea7a4258c514f060f53b9
KDH <silverstuff@naver.com>
2024-02-14 17:36:25 +0900

    fix : livemanage/alert
    Former-commit-id: 55eb9940eb5b808447c35432290774a1cba818e6

--- frontend/coachme/src/views/mypages/coach/LiveManageView.vue
+++ frontend/coachme/src/views/mypages/coach/LiveManageView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10cd7adcbead581e4a758f2a860aecea91da33c5e186e61e9184116714784033
-size 9974
+oid sha256:6262d3b40a4f954f02ff1d9a6e9581f335ea0bd3936addfbaea15f24fd3746c1
+size 10422
