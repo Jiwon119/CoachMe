13c1cb1f1094ec0bb1593b3e3bab158da22e358a
김지원 <wldnjs35242@naver.com>
2024-02-14 19:25:24 +0900

    Update DmSocketController.java
    Former-commit-id: 37e5513471fa903725c1141de5af29b3be5a2d58

--- backend/src/main/java/com/ssafy/api/dm/controller/DmSocketController.java
+++ backend/src/main/java/com/ssafy/api/dm/controller/DmSocketController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c5ffc6fb25b22b742b178667829e80e6bdbdf38b18c73b211a110ad676031e09
-size 1398
+oid sha256:29a8da930a883eef1658149538a8f0c6009a3fd1d3ee7e3064c28330c567d18d
+size 1787
