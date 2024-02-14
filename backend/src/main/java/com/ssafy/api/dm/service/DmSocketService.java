e23f2284c9c5c338fa35f3d40df964b4468fc026
김지원 <wldnjs35242@naver.com>
2024-02-14 09:40:07 +0900

    feat: 라이브 코칭 녹화 api 구현
    Former-commit-id: ef67c736e0a06eaa236b8d0a1cd9f0b2d6987238 [formerly 18daccb6b5f588a5e67d63a18ff03899d3dd1597]
    Former-commit-id: 3b34979a8ee3f6a2b900bdf47c595c1f0c96d68d

--- backend/src/main/java/com/ssafy/api/dm/service/DmSocketService.java
+++ backend/src/main/java/com/ssafy/api/dm/service/DmSocketService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4cedeeb450e0f7234a55721591d65cada30dd64a1534fb98dd156c8ccb0db899
-size 1907
+oid sha256:ef2d7e21986eaf4298a38cebdec70a3c3db2edde65c3b8bb32623f2546f6e9c0
+size 1741
---
+++ backend/src/main/java/com/ssafy/api/livecoaching/dto/response/LiveCoachingMemberResponseDto.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3fff916c0ce9f0797621f7907b759e1066666b2312b740e671c52ae0bda4c192
+size 237
