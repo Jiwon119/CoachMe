893de5230b6649ece7d8e9a13d1edd7ac5723391
Sangyeop Nam <spearoad15@gmail.com>
2024-02-14 13:05:27 +0900

    feat : 화상회의
    Former-commit-id: 198055df63bbed252eaa0fecb5d6643dea443188

--- frontend/coachme/src/components/openvidu/UserVideo.vue
+++ frontend/coachme/src/components/openvidu/UserVideo.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc4c13c77a151cda80df251dc2a734e9e0c87af0014c62ff6be4719bbd4b3313
-size 624
+oid sha256:2bc244443def8187454957e8187303c543a627cc425c8028763fc1fbaa3c07fd
+size 573
