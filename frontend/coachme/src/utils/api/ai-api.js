d41b465cbc7feeb90678d6f65ca64ba234384c62
SangyeopNam <spearoad15@gmail.com>
2024-02-14 21:56:58 +0900

    fix : cors 추가
    Former-commit-id: e8b3c35a860b2ad05955ec076d372a3021f29d74 [formerly edbd2960c588078f76fddc7911840b05fd19c4d4]
    Former-commit-id: 0af0e21c257bfd7e513813ad28e9dc2e40057364

--- frontend/coachme/src/utils/api/ai-api.js
+++ frontend/coachme/src/utils/api/ai-api.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f716f6df43bac9dd38ec73ef5104ec312827bbeacd0ebfd72dcd5ef4b758d77
-size 684
+oid sha256:3700ee838b7099c1f2ef6107a29e4adf5b4bdf0c27f7fcb8a794e66d8137939d
+size 807
