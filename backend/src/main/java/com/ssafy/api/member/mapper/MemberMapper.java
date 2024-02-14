8676824a1cdb570557a78577bb77c445f2477fbc
김지원 <wldnjs35242@naver.com>
2024-02-14 12:23:32 +0900

    fix : openVidu member profileText 추가
    Former-commit-id: c9f38c76494dd5dea5aead86d400cebd68332f8a

--- backend/src/main/java/com/ssafy/api/member/mapper/MemberMapper.java
+++ backend/src/main/java/com/ssafy/api/member/mapper/MemberMapper.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5020e4eeae5755962aa1e4064e9bf2200bd1136ceb4075e2be9c68e3373e330e
-size 1946
+oid sha256:07efd8a9ef6c325342c02e48e60ef045837c4247e409266871d80208e06e4d67
+size 2011
