7a19154b78b980d0a5ad36b284e4ac9f85e68636
SangyeopNam <spearoad15@gmail.com>
2024-02-14 21:00:44 +0900

    feat : 포기
    Former-commit-id: 29e75a398fa23d11640c687e906fce383887647d [formerly 97136a3a15dceba9848f1e3c8a772f61529ae236]
    Former-commit-id: 22ab27cfe26e1b2c6e1e582edae04de7c45b8c1f

--- frontend/coachme/src/views/LiveRoom.vue
+++ frontend/coachme/src/views/LiveRoom.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72e55e3717344be27015fa3407a5166f990598333c224b4344d93a18e7eabb16
-size 15443
+oid sha256:fd3f41c1a5b5f179a4e97ee0d3050be0610d58b67467f93f3cc0d85f8c08ccbf
+size 15431
