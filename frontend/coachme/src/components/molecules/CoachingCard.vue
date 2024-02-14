dca47027179c0497cbabee789190388f617a7803
SangyeopNam <spearoad15@gmail.com>
2024-02-14 22:01:24 +0900

    fix : cors 추가
    Former-commit-id: 2a5368b829c9e85f2c16a764e963dccd4495cf62 [formerly d0044506dd647a2a22f1516eeb6cce22b4679ba9]
    Former-commit-id: d09c91bb297b6d5bf29fc9887b9bf30f7e66462b

--- frontend/coachme/src/components/molecules/CoachingCard.vue
+++ frontend/coachme/src/components/molecules/CoachingCard.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d1eea3556290b845044e835b46bffca07adc7fb58d3f9d723fba6d9ee9beee4
-size 2888
+oid sha256:d82ce6cb93068b963466bdc2b1defb24970e42bb8095d348c63e1d967e15a513
+size 2952
