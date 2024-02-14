b96c2eff81d57323785a6fedbc602235bf2a8f3f
김지원 <wldnjs35242@naver.com>
2024-02-14 11:52:15 +0900

    Update LiveCoachingController.java
    Former-commit-id: 496f434d8cc33eeaea28690eafeb3a8a527df04e

--- backend/src/main/java/com/ssafy/api/livecoaching/controller/LiveCoachingController.java
+++ backend/src/main/java/com/ssafy/api/livecoaching/controller/LiveCoachingController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e430d1ac73854d8254a10398549c5693974e40e542f662bfaf007a0fac278e7a
-size 6636
+oid sha256:36481572b4fc712ee2c3c3cec1add5bc995a10e56a8e1fc971f567dc101a55e6
+size 6626
