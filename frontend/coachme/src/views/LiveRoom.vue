53c8b7be89c08ee1160651bce79e321e0a625e87
Sangyeop Nam <spearoad15@gmail.com>
2024-02-14 15:45:47 +0900

    fix : node env
    Former-commit-id: 3b582b151cdf89c95656f3bd22d522fcaa3d781d

--- frontend/coachme/src/views/LiveRoom.vue
+++ frontend/coachme/src/views/LiveRoom.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40a69ebb4e2cf3bcf11068e0efaf945d8fac4a267454dc146ce2b774dd3b8a71
-size 14040
+oid sha256:711b57b1982738fd549a9c9c37445ba13c5fd1abf1a1ff0ab29274ac5ea4f876
+size 14150
