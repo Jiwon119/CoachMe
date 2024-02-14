8368cfeece9b696eb9bf195ff727f3a287780202
Sangyeop Nam <spearoad15@gmail.com>
2024-02-14 11:01:30 +0900

    save
    Former-commit-id: 36652fa97940d06a7232f31517367032e4e05516 [formerly 2cda39aa3907774973ddceb4fb72acac8ca4fac1]
    Former-commit-id: 4e1e4f7d0286a20edbf02897fdcccf793fbbc59a

---
+++ frontend/coachme/src/components/molecules/videoManage/VideoUploader.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:028e86a708e4110de2688f72a6a08d538b59d9aae901932017b4efb97657d10c
+size 503
--- frontend/coachme/src/views/mypages/coach/VideoManageView.vue
+++ frontend/coachme/src/views/mypages/coach/VideoManageView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a89af4b9773f24d47c03f75253ca0725a6cae9444ebe2681fbf3b09827008200
-size 5373
+oid sha256:55cc41fe3ac4df608e27cc3f2b88514321a819208596ff41043e85b128bda26a
+size 6708
