741e77c95f92222f9697cd0e6a1cf3ed10ce1e03
이준학 <0710june@naver.com>
2024-02-16 01:06:05 +0900

    fix: coaching-9, porting manual 수정
    Former-commit-id: 5ccf5078a5cc9ac64d603296fa03e5c3861fd069 [formerly 76671d63c8952a4da0dc240433f181aaa7911de0]
    Former-commit-id: fe21304ec5214574c0a15eb2b465724f9c47db4e

--- backend/src/main/java/com/ssafy/api/coaching/dto/request/VideoRequestDto.java
+++ backend/src/main/java/com/ssafy/api/coaching/dto/request/VideoRequestDto.java
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3ce7de2b0ba9fff9e4850a2ccc98041b00e9f0d5b40b83176bcb4b12cfed21c9
-size 177
+package com.ssafy.api.coaching.dto.request;
+
+import lombok.Data;
+
+@Data
+public class VideoRequestDto {
+
+  private Long coachId;
+  private String videoName;
+  private String url;
+
+}
