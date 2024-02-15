25a1b69d0f21385262cd8dd4d76729595c7907fe
Sangyeop Nam <spearoad15@gmail.com>
2024-02-15 15:09:04 +0900

    fix : 화상회의
    Former-commit-id: f5e58c8353a73f74e75f59dfa6a65397face2386

--- frontend/coachme/src/views/LiveRoom.vue
+++ frontend/coachme/src/views/LiveRoom.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d67048efb00940d1b7112e9c8132b0307f7053e7a690b4a49894008518d3dc1
-size 19608
+oid sha256:2eb05e81a2accba5d45ad8cdf17dbcdbb63cc18ff464411209681ef0d0f6b525
+size 19265
