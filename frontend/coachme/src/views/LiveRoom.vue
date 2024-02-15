151dfe5d1229e0ea5d43891282db2ff9bc46f3e3
Sangyeop Nam <spearoad15@gmail.com>
2024-02-15 13:36:03 +0900

    feat : 화상회의좀 되세요 제발
    Former-commit-id: 05538d3192f03c6e58560e70fba62b89dfe0e025

--- frontend/coachme/src/views/LiveRoom.vue
+++ frontend/coachme/src/views/LiveRoom.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f5dc4c9050001808ff474763675b5570dea56246e5cd072992d9d04bcaf10138
-size 18372
+oid sha256:cc6b3a3116caf89e4b25da423a87dab8b7def14b8d084ea2adac2bc1a678d3d2
+size 18997
