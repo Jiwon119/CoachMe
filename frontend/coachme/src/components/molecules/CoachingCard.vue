c3dfb7a80695b1708daa38630368c77116d2eefa
SangyeopNam <spearoad15@gmail.com>
2024-02-15 20:03:21 +0900

    log정리
    Former-commit-id: de42b24a423d9e14232a81eacb8d0b097965e431

--- frontend/coachme/src/components/molecules/CoachingCard.vue
+++ frontend/coachme/src/components/molecules/CoachingCard.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93388f3a51012001df6cd07e413a9db29f0e7bc330d35e16a891b65cf08a251b
-size 3073
+oid sha256:2e4bde6afa1e1759650615ae57506131a44b50bc5c535923fb96d3144aa26e26
+size 3068
