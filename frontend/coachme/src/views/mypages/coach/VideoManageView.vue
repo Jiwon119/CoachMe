0a0e5d9501465ce44c73c6c8016a66e912c78ea6
Sangyeop Nam <spearoad15@gmail.com>
2024-02-16 08:47:23 +0900

    fix : 레이아웃 수정
    Former-commit-id: d780dc1185175a809ff3ba57640c9a2639761e8a [formerly f1ce11ae6db571a56ffa3b2c5739f79fbfa51a85]
    Former-commit-id: da9dde77db86fa073214216c5cb5ebc69b73fafd

--- frontend/coachme/src/views/mypages/coach/VideoManageView.vue
+++ frontend/coachme/src/views/mypages/coach/VideoManageView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db59ad472e396d00ced9b658d04773d7d0ee26a1f77c80141e83c0fc67493427
-size 11319
+oid sha256:6e3b9fb802eb61c6b4bed489112a292d075020f4d4b392c60bbe120be1dfec6d
+size 11476
