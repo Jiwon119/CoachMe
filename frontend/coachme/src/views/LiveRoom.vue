151e29b7a423b67345efe034be4865634774d048
Sangyeop Nam <spearoad15@gmail.com>
2024-02-15 13:51:41 +0900

    fix
    Former-commit-id: 51d6e9d20d20338d3961cd704128f69418e122db [formerly 3069b6e07e9d754997dafb792777cb2f6ae942fd]
    Former-commit-id: 1c3648bf313138d111bc10c3c51c3334550edd44

--- frontend/coachme/src/views/LiveRoom.vue
+++ frontend/coachme/src/views/LiveRoom.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:450753de6a9ad5366acbc1a2272dfe98d778bf3c77422d322752cc12c2004577
-size 19528
+oid sha256:68964193650a9cf0d99c877d3f79a2a088b882dd9b6f952d5753c19df0fd78f9
+size 19598
