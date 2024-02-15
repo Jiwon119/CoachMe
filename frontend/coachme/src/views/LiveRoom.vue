9de2b3e7a48c854dfb7eabd680ce3d6496f0d4ec
Sangyeop Nam <spearoad15@gmail.com>
2024-02-15 16:13:25 +0900

    fix : 참가자 목록 늘어나는거 수정
    Former-commit-id: 64211fe5ee2c1d6183ddee375f387f187ce81b3f

--- frontend/coachme/src/views/LiveRoom.vue
+++ frontend/coachme/src/views/LiveRoom.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52abed64e0801790e1b5ca7c06d657afab3d0da2bef72789fa7e07085031d03f
-size 19489
+oid sha256:fbd919f1f85b353ce51bb25ca6e4696d04aaf30d3e8d71ad7e1b637e6775e62a
+size 19759
