92c464f99319d59fcd28636fc418e50812f7a9f2
KDH <silverstuff@naver.com>
2024-02-15 09:06:52 +0900

    fix icon
    Former-commit-id: 1029487296a303fbe51ad541c988a6ac11aaf34b

--- frontend/coachme/src/views/search/coach/CoachDetail.vue
+++ frontend/coachme/src/views/search/coach/CoachDetail.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a24a309713815bb81464d9fe659fb2f221c37b8627ca6d82e68411263af22727
-size 13685
+oid sha256:c41000a1a26106e00fc8101fd3d34dad11cb587b29ccce4e3c606683e67ebbb3
+size 13688
