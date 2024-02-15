0992fcb933533bf15ee52ae15bdc5dfc0993f501
Sangyeop Nam <spearoad15@gmail.com>
2024-02-15 15:36:50 +0900

    fix : 화면전환
    Former-commit-id: c8f91cd5a3437f93f062153140ae8d8d7418f82f [formerly f28af09470eec18a4d336af0bae9c0ebcc17b364]
    Former-commit-id: 7877453c9edcd77ca79384f0db6d7791e4a700a3

--- frontend/coachme/src/views/LiveRoom.vue
+++ frontend/coachme/src/views/LiveRoom.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2eb05e81a2accba5d45ad8cdf17dbcdbb63cc18ff464411209681ef0d0f6b525
-size 19265
+oid sha256:52abed64e0801790e1b5ca7c06d657afab3d0da2bef72789fa7e07085031d03f
+size 19489
