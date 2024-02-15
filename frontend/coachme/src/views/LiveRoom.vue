10423fb72cbf4d15fa196f8f92ba5d2a62cdd43c
Sangyeop Nam <spearoad15@gmail.com>
2024-02-15 12:40:13 +0900

    fix : 코미배열 로직 수정
    Former-commit-id: dbba97b2351c592fd22e8a04626a1a9e46c2b5f2 [formerly 3292dfa4241d52d68123a78af607af5ecfebe913]
    Former-commit-id: ec1b185d66732e1fae5235be695b6e164c59e2ba

--- frontend/coachme/src/views/LiveRoom.vue
+++ frontend/coachme/src/views/LiveRoom.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eaa20c7aa0ec1d29a26a57043b9a197dc5cb602c916a0283ac5a1cb3cb1d1384
-size 17511
+oid sha256:79063072952f3064f92681dc7df7b27f168afeff2e53b87fb6dde0a0ba7b16e3
+size 17875
