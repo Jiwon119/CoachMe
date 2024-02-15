92a469c0ac01598734c5ac951970d3a4177c77ee
KDH <silverstuff@naver.com>
2024-02-15 20:21:13 +0900

    fix : chatting alert
    Former-commit-id: 0f9581b402a834f419f85368dae998decaf06dba [formerly 2835da0173f5c6c495112f7aa3b0e76c3a588962]
    Former-commit-id: 20289d571e5562e4ada608546c493403f4f1f474

--- frontend/coachme/src/components/openvidu/OvVideo.vue
+++ frontend/coachme/src/components/openvidu/OvVideo.vue
@@ -1,3 +1,17 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e55ad16c720a58c55958cb4bee9f825e72d18ccb3fad772f69e9de6b13b46518
-size 247
+<template>
+  <video autoplay style="width: 100%; height: 100%" />
+</template>
+
+<script>
+export default {
+  name: 'OvVideo',
+
+  props: {
+    streamManager: Object
+  },
+
+  mounted() {
+    this.streamManager.addVideoElement(this.$el)
+  }
+}
+</script>
