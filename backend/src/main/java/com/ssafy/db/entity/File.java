9f89c836125038a18e70a28cb0cbf705854e77e2
이준학 <0710june@naver.com>
2024-02-16 10:07:45 +0900

    fix: ai 편집 영상 저장 이름 수정
    Former-commit-id: f7d8bbd6f1dbb683e575c3eaa66812d556977ae2

--- backend/src/main/java/com/ssafy/db/entity/File.java
+++ backend/src/main/java/com/ssafy/db/entity/File.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:787befa76ebeec12884974cf4a767be9d6b186824dff798543dedea3afc8d3b6
-size 991
+oid sha256:6834d3edee87e30158e194b8b04c8744c8dcd51e78b85f2c7defcf0ff2ff287e
+size 983
