a31534760d26974dbc073429df4ea011d2affdc2
Sangyeop Nam <spearoad15@gmail.com>
2024-02-16 09:00:25 +0900

    fix
    Former-commit-id: cd0748351fc8487dde95010bfacd07f877e30762

--- frontend/coachme/src/views/mypages/coach/VideoManageView.vue
+++ frontend/coachme/src/views/mypages/coach/VideoManageView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e3b9fb802eb61c6b4bed489112a292d075020f4d4b392c60bbe120be1dfec6d
-size 11476
+oid sha256:3c57f9888acbfdcbfbf0213663552611c65134a05966d8c133d9ddcbd142e05b
+size 11544
